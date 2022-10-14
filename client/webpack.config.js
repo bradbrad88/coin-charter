29d89a1f5ed9d1343ef84c42dad1153ed2a5a148
Brad Teague <b_rad88@live.com>
2022-10-14 15:45:29 +1030

    Setup React with Typescript and Tailwind

---
+++ client/webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const path = require("path");
+const dotenv = require("dotenv");
+
+const env = dotenv.config().parsed;
+
+const envKeys = Object.keys(env).reduce((p, key) => {
+  p[`process.env.${key}`] = JSON.stringify(env[key]);
+  return p;
+}, {});
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  target: "web",
+  devServer: {
+    port: "3000",
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    watchFiles: ["src/**/*.{js,ts,tsx,jsx}", "public/**/*"],
+    open: true,
+    hot: "only",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    alias: {
+      src: path.resolve(__dirname, "src/"),
+      components: path.resolve(__dirname, "src/components/"),
+      hooks: path.resolve(__dirname, "src/hooks/"),
+      contexts: path.resolve(__dirname, "src/contexts/"),
+      assets: path.resolve(__dirname, "src/assets/"),
+    },
+  },
+  module: {
+    // Apply to .ts or .tsx files
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      { test: /\.(tsx?)$/, loader: "ts-loader" },
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: "babel-loader" },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin(envKeys),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+  stats: "minimal",
+};
