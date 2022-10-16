e6e7143aa50e584bb0c218aef015e2c5ebae767f
Brad Teague <b_rad88@live.com>
2022-10-16 15:50:32 +1030

    Create basic routing for 4 main pages in application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Homepage from "pages/Homepage";
+import Friends from "pages/Friends";
+import Dashboard from "pages/Dashboard";
+import Charts from "pages/Charts";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route index element={<Homepage />} />
+      <Route path="friends" element={<Friends />} />
+      <Route path="dashboard" element={<Dashboard />} />
+      <Route path="charts" element={<Charts />} />
+    </Routes>
+  );
 };
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "src/App";
 import "./main.css";
@@ -7,4 +8,8 @@ import "./main.css";
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(
+  <Router>
+    <App />
+  </Router>,
+);
