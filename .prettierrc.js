7090a9bbf2353c1fe3eea03ea0423ad445a84407
Brad Teague <b_rad88@live.com>
2022-10-16 11:32:22 +1030

    Eslint and Prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+
+module.exports = {
+  tabWidth: 2,
+  semi: true,
+  trailingComma: "all",
+  singleQuote: false,
+  arrowParens: "always",
+  useTabs: false,
+};
