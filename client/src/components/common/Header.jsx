419daa2e2ee870dba7ee35af4b019efa7bb7e072
Sammy <marchsam1988@gmail.com>
2022-10-17 12:19:49 +1030

    nav bar skeleton ready for styling

---
+++ client/src/components/common/Header.jsx
@@ -0,0 +1,19 @@
+import NavItem from "./NavItem";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between bg-orange-300 p-8">
+      <Link to={"/"}>
+        <h1 className="text-5xl">Header</h1>
+      </Link>
+      <nav className="self-end flex gap-5">
+        <NavItem path={"/"} title={"Home"} />
+        <NavItem path={"/friends"} title={"Friends"} />
+        <NavItem path={"/charts"} title={"Charts"} />
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
