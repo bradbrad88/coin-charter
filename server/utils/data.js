efe4a7f8893d90a2fb844b20464953aeea97a4bd
Ben Smerd <bsmerd@adam.com.au>
2022-10-23 14:37:15 +1030

    added in subtitle for Users

--- server/utils/data.js
+++ server/utils/data.js
@@ -1,12 +1,14 @@
 const userSeed = [
   {
     username: "Bennyboy",
+    subTitle: "Beginner Crypto",
     email: "bennyboy@google.com",
     bio: "I am a beginner trader and enjoy searching up new crypto coins",
     favCoins: ["QNT", "ETH", "DAG"],
   },
   {
     username: "Braddy",
+    subTitle: "Lover of coins",
     email: "braddy@test.com",
     bio: "I am very excited to learn more about how charts work and start using them more.",
     favCoins: ["ADA", "BNB"],
