ca4b53d03964e930a6945dc0f10b1ad6df41d517
Brad Teague <b_rad88@live.com>
2022-10-24 22:04:06 +1030

    Create CoinList component that displays a table of coin data and render in Homepage

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,5 +1,11 @@
+import CoinList from "src/components/features/coins/CoinList";
+
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className="mx-auto mt-5 max-w-screen-lg border-[1px] rounded-sm shadow-md">
+      <CoinList />
+    </div>
+  );
 };
 
 export default Homepage;
