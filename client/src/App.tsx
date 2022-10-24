7c89d707ba165710fa2cca90637068d7dec4a823
Ben Smerd <bsmerd@adam.com.au>
2022-10-24 15:46:34 +1030

    created coin profile card component and styled with tailwind for layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,16 +4,19 @@ import Friends from "pages/Friends";
 import Dashboard from "pages/Dashboard";
 import Charts from "pages/Charts";
 import Header from "common/Header";
+import CoinProfile from "pages/CoinProfile";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Routes>
-        <Route index element={<Homepage />} />
+        {/* <Route index element={<Homepage />} /> */}
         <Route path="friends" element={<Friends />} />
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="charts" element={<Charts />} />
+        // !!! DELETE AFTER DONE THE ACTUAL PAGE DONE UP
+        <Route index element={<CoinProfile />} />
       </Routes>
     </div>
   );
---
+++ client/src/pages/CoinProfile.tsx
@@ -0,0 +1,11 @@
+import CoinProfileCard from "../components/features/coinProfile/CoinProfileCard";
+
+const CoinProfile = () => {
+  return (
+    <div>
+      <CoinProfileCard />
+    </div>
+  );
+};
+
+export default CoinProfile;
