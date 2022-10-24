022a96d29db0e03657893d7f390ce6904491acfb
Sammy <marchsam1988@gmail.com>
2022-10-24 16:42:32 +1030

    added some styling to the header/added favorites

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,9 +1,11 @@
 import ProfileCard from "src/components/features/dashboard/ProfileCard";
+import Favorites from "src/components/features/dashboard/Favorites";
 
 const Dashboard = () => {
   return (
-    <div>
+    <div className="flex">
       <ProfileCard />
+      <Favorites />
     </div>
   );
 };
