59267e76e001d8456aba0d2ae998af9bdcd30ee8
Ben Smerd <bsmerd@adam.com.au>
2022-10-28 10:23:32 +1030

    added typeDefs queries

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,16 @@
 const { gql } = require("apollo-server-express");
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type Query {
+    users: [Users]
+    comments: [Comments]
+    coins: [Coins]
+    charts: [Charts]
+    user(user: ID!): Users
+    comment(comment: ID!): Comments
+    coin: Coins
+    chart: Charts
+  }
+`;
 
 module.exports = typeDefs;
