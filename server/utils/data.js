dcb41650b06384c65e2e682cbd7207199d042837
Ben Smerd <bsmerd@adam.com.au>
2022-10-28 10:44:42 +1030

    added in password for the Users model and to the data seed

--- server/utils/data.js
+++ server/utils/data.js
@@ -1,6 +1,7 @@
 const userSeed = [
   {
     username: "Bennyboy",
+    password: "12345678",
     subTitle: "Beginner Crypto",
     email: "bennyboy@google.com",
     bio: "I am a beginner trader and enjoy searching up new crypto coins",
@@ -8,6 +9,7 @@ const userSeed = [
   },
   {
     username: "Braddy",
+    password: "12345678",
     subTitle: "Lover of coins",
     email: "braddy@test.com",
     bio: "I am very excited to learn more about how charts work and start using them more.",
@@ -15,6 +17,7 @@ const userSeed = [
   },
   {
     username: "Sammy",
+    password: "12345678",
     email: "sammy@google.com",
     bio: "Crypto is very interesting and i am keen to get into the space.",
     favCoins: ["BTC"],
