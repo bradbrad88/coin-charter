6734b47e7555b5e2b8507df48fa4d4f243b3f1cf
Ben Smerd <bsmerd@adam.com.au>
2022-10-28 13:37:10 +1030

    added in query resolvers for users, coins, comments, charts

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,7 +2,42 @@ const { Charts, Coins, Comments, Users } = require("../models");
 
 //! DONT FOGET TO POPULATE WITH SUBDOCS FOR MOST
 const resolvers = {
-  Query: {},
+  Query: {
+    users: async () => {
+      // ? not sure if populate with Users again because of self-reference
+      return await Users.find({}).populate("comments");
+    },
+    user: async (parent, args) => {
+      return await Users.findById(args.id).populate("comments");
+    },
+    comments: async () => {
+      return await Comments.find({})
+        .populate("users")
+        .populate({ path: "users", populate: "comments" });
+    },
+    comment: async (parent, args) => {
+      return await Comments.findById(args.id)
+        .populate("users")
+        .populate({ path: "users", populate: "comments" });
+    },
+    coins: async (parent, args) => {
+      return await Coins.findById(args.id)
+        .populate("comments")
+        .populate({ path: "comments", populate: "users" })
+        .populate("charts")
+        .populate({ path: "charts", populate: "comments" });
+    },
+    charts: async () => {
+      return await Charts.find({})
+        .populate("comments")
+        .populate({ path: "comments", populate: "users" });
+    },
+    chart: async (parent, args) => {
+      return await Charts.findById(args.id)
+        .populate("comments")
+        .populate({ path: "comments", populate: "users" });
+    },
+  },
   Mutation: {},
 };
 
