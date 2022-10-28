78a52993dd2116198e576ae253c95fad88ebdee4
Ben Smerd <bsmerd@adam.com.au>
2022-10-28 11:10:10 +1030

    added in Users typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { Charts, Coins, Comments, Users } = require("../models");
 
+//! DONT FOGET TO POPULATE WITH SUBDOCS FOR MOST
 const resolvers = {
   Query: {},
   Mutation: {},
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,19 @@
 const { gql } = require("apollo-server-express");
 
+//TODO change around models and then change queries to match
+
 const typeDefs = gql`
+  type Users {
+    _id: ID
+    username: String
+    subTitle: String
+    email: String
+    bio: String
+    comments: [Comments]
+    friends: [Users]
+    favCoins: [String]
+  }
+
   type Query {
     users: [Users]
     comments: [Comments]
