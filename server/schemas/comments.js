d6b78899ea1bc32e5bae167457d59c78faee93c3
Ben Smerd <bsmerd@adam.com.au>
2022-10-28 12:27:51 +1030

    temporary change back to singular module

--- server/schemas/comments.js
+++
@@ -1,7 +0,0 @@
-export const typeDef = `
-  type Comments {
-    _id: ID
-    commentText: String
-    createdAt: 
-  }
-`;
---
+++ server/schemas/typeDef.js
@@ -0,0 +1,35 @@
+const { gql } = require("apollo-server-express");
+
+// TODO create a scalar for date type
+
+const typeDefs = gql`
+  type Users {
+    _id: ID
+    username: String
+    subTitle: String
+    email: String
+    bio: String
+    comments: [Comments]
+    friends: [Users]
+    favCoins: [String]
+  }
+
+  type Comments {
+    _id: ID
+    commentText: String
+    createdAt: Date
+  }
+
+  type Query {
+    users: [Users]
+    comments: [Comments]
+    coins: [Coins]
+    charts: [Charts]
+    user(user: ID!): Users
+    comment(comment: ID!): Comments
+    coin(coin: ID!): Coins
+    chart(chart: ID!): Charts
+  }
+`;
+
+module.exports = typeDefs;
