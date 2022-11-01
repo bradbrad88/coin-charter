2f703c07cbcb33d0a5e5ba630acbd62134bb4806
Sammy <marchsam1988@gmail.com>
2022-11-01 17:22:10 +1030

    responsive header and navbar and styling

--- client/src/components/common/Header.jsx
+++ client/src/components/common/Header.jsx
@@ -1,20 +1,49 @@
-import NavItem from "./NavItem";
 import { Link } from "react-router-dom";
+import NavBar from "./NavBar";
+import HamburgerButton from "./HamburgerButton";
+import { useState, useEffect } from "react";
+import SearchBar from "./SearchBar";
+import { TbArrowBackUp } from "react-icons/tb";
 
 const Header = () => {
+  const [hideNav, setHideNav] = useState(true);
+  useEffect(() => {
+    const onResize = () => {
+      setHideNav(true);
+    };
+    window.addEventListener("resize", onResize);
+    return () => window.removeEventListener("resize", onResize);
+  }, []);
   return (
-    <div className="flex justify-between p-5 border mt-8">
-      <Link to={"/"}>
-        <h1 className="text-5xl pl-72">Coin Charter</h1>
+    <div className="flex lg:justify-between justify-center p-5 border-b relative">
+      <div className="flex lg:hidden absolute left-0 pl-5">
+        <HamburgerButton onClick={() => setHideNav(false)} />
+      </div>
+      <Link to={"/"} className="center">
+        <h1 className="text-xl whitespace-nowrap text-center">
+          Coin Charter pp
+        </h1>
       </Link>
-      <nav className="self-end flex gap-5 pr-72">
-        <NavItem path={"/"} title={"Home"} />
-        <NavItem path={"/friends"} title={"Friends"} />
-        <NavItem path={"/charts"} title={"Charts"} />
-        <NavItem path={"/dashboard"} title={"Dashboard"} />
-        {/*temporary in here while dev */}
-        <NavItem path={"/coin"} title={"Coin"} />
-      </nav>
+      <div
+        className={`${
+          hideNav ? "" : "translate-x-full"
+        } fixed overflow-hidden top-0 bottom-0 left-[-100vw] right-[100vw] transition-transform bg-white lg:relative lg:flex lg:translate-x-0 lg:transition-none lg:left-0`}
+      >
+        <div className="flex flex-col gap-3 bg-indigo-800 bg-opacity-80 p-6 h-full w-full lg:p-0 lg:bg-white">
+          <div className="flex lg:hidden">
+            <button
+              className="ml-auto translate-x-2 -translate-y-2"
+              onClick={() => setHideNav(true)}
+            >
+              <TbArrowBackUp color="white" size={24} />
+            </button>
+          </div>
+          <div className="flex flex-col lg:flex-row-reverse gap-5 lg:items-center">
+            <SearchBar />
+            <NavBar close={() => setHideNav(true)} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- client/src/components/common/NavItem.jsx
+++ client/src/components/common/NavItem.jsx
@@ -1,16 +1,17 @@
 import { Link, useLocation } from "react-router-dom";
 
-const NavItem = ({ title, path }) => {
+const NavItem = ({ title, path, onClick }) => {
   const location = useLocation();
   const active = location.pathname.split("/")[1] === path.replace("/", "");
   return (
     <Link
-      className={`hover:text-indigo-600 hover:opacity-80 transition-colors ${
-        active ? "text-xl" : ""
+      className={`hover:translate-x-1 transition-all duration-300 ${
+        active ? "" : ""
       }`}
       to={path}
+      onClick={onClick}
     >
-      {title}
+      <div className={`w-fit ${active ? "border-b" : ""}`}>{title}</div>
     </Link>
   );
 };
