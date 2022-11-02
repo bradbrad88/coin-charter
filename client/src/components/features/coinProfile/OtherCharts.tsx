eeda69e34b3b7cb1a290bf6d9aa413a587a6023c
Ben Smerd <bsmerd@adam.com.au>
2022-11-02 18:46:20 +1030

    fixed up layout for charts

--- client/src/components/features/coinProfile/OtherCharts.tsx
+++ client/src/components/features/coinProfile/OtherCharts.tsx
@@ -1,7 +1,18 @@
 const OtherCharts = () => {
+  const filterOptions = ["Most Recent", "Top Rated", "Least Rated", "Oldest"];
   return (
-    <div>
-      <h1>Most Recent Charts</h1>
+    <div className="flex flex-col">
+      <div className="flex gap-2">
+        {/* will need to add in the state for the filter choice */}
+        <h1 className="text-lg font-bold">Most Recent Charts</h1>
+        <select value="most recent">
+          {filterOptions.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 };
