965c22f2d97eade40f4f396a2650b350c6218bf2
Ben Smerd <bsmerd@adam.com.au>
2022-11-02 16:39:20 +1030

    added in comments section for coin profile

--- client/src/components/features/coinProfile/CoinComments.tsx
+++ client/src/components/features/coinProfile/CoinComments.tsx
@@ -1,7 +1,351 @@
 const CoinComments = () => {
   return (
-    <div className="flex flex-col rounded-sm gap-5 shadow-lg shadow-gray-400 m-5 p-5 w-[400px] h-[400px]">
-      <h1 className="text-lg font-bold">Coin comments</h1>
+    <div className="flex flex-col rounded-sm gap-1 shadow-lg shadow-gray-400 m-5 p-5 w-[400px] h-screen">
+      <h1 className="text-lg font-bold">Comments</h1>
+      <ul className="flex flex-col gap-1 overflow-y-scroll">
+        <li className="border-black-50 border-solid border-2 flex gap-2 width-5/6 h-[80px]">
+          <div className="flex flex-col items-center w-1/6 gap-2">
+            <img
+              src="https://source.unsplash.com/random/?person/"
+              className="h-[50px] w-[50px] rounded"
+            />
+            <div className="flex gap-2 items-center">
+              <i className="fa-regular fa-thumbs-up"></i>
+              <i className="fa-regular fa-thumbs-down"></i>
+            </div>
+          </div>
+          <div className=" flex flex-col w-5/6 h-5/6 border-black border-1 border-solid">
+            <h3 className="font-bold text-sm">Jamie</h3>
+            <p className="text-xs">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              nesciunt ipsum, voluptatibus odio labore libero!
+            </p>
+          </div>
+        </li>
+        <li className="border-black-50 border-solid border-2 flex gap-2 width-5/6 h-[80px]">
+          <div className="flex flex-col items-center w-1/6 gap-2">
+            <img
+              src="https://source.unsplash.com/random/?person/"
+              className="h-[50px] w-[50px] rounded"
+            />
+            <div className="flex gap-2 items-center">
+              <i className="fa-regular fa-thumbs-up"></i>
+              <i className="fa-regular fa-thumbs-down"></i>
+            </div>
+          </div>
+          <div className=" flex flex-col w-5/6 h-5/6 border-black border-1 border-solid">
+            <h3 className="font-bold text-sm">Jamie</h3>
+            <p className="text-xs">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              nesciunt ipsum, voluptatibus odio labore libero!
+            </p>
+          </div>
+        </li>
+        <li className="border-black-50 border-solid border-2 flex gap-2 width-5/6 h-[80px]">
+          <div className="flex flex-col items-center w-1/6 gap-2">
+            <img
+              src="https://source.unsplash.com/random/?person/"
+              className="h-[50px] w-[50px] rounded"
+            />
+            <div className="flex gap-2 items-center">
+              <i className="fa-regular fa-thumbs-up"></i>
+              <i className="fa-regular fa-thumbs-down"></i>
+            </div>
+          </div>
+          <div className=" flex flex-col w-5/6 h-5/6 border-black border-1 border-solid">
+            <h3 className="font-bold text-sm">Jamie</h3>
+            <p className="text-xs">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              nesciunt ipsum, voluptatibus odio labore libero!
+            </p>
+          </div>
+        </li>
+        <li className="border-black-50 border-solid border-2 flex gap-2 width-5/6 h-[80px]">
+          <div className="flex flex-col items-center w-1/6 gap-2">
+            <img
+              src="https://source.unsplash.com/random/?person/"
+              className="h-[50px] w-[50px] rounded"
+            />
+            <div className="flex gap-2 items-center">
+              <i className="fa-regular fa-thumbs-up"></i>
+              <i className="fa-regular fa-thumbs-down"></i>
+            </div>
+          </div>
+          <div className=" flex flex-col w-5/6 h-5/6 border-black border-1 border-solid">
+            <h3 className="font-bold text-sm">Jamie</h3>
+            <p className="text-xs">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              nesciunt ipsum, voluptatibus odio labore libero!
+            </p>
+          </div>
+        </li>
+        <li className="border-black-50 border-solid border-2 flex gap-2 width-5/6 h-[80px]">
+          <div className="flex flex-col items-center w-1/6 gap-2">
+            <img
+              src="https://source.unsplash.com/random/?person/"
+              className="h-[50px] w-[50px] rounded"
+            />
+            <div className="flex gap-2 items-center">
+              <i className="fa-regular fa-thumbs-up"></i>
+              <i className="fa-regular fa-thumbs-down"></i>
+            </div>
+          </div>
+          <div className=" flex flex-col w-5/6 h-5/6 border-black border-1 border-solid">
+            <h3 className="font-bold text-sm">Jamie</h3>
+            <p className="text-xs">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              nesciunt ipsum, voluptatibus odio labore libero!
+            </p>
+          </div>
+        </li>
+        <li className="border-black-50 border-solid border-2 flex gap-2 width-5/6 h-[80px]">
+          <div className="flex flex-col items-center w-1/6 gap-2">
+            <img
+              src="https://source.unsplash.com/random/?person/"
+              className="h-[50px] w-[50px] rounded"
+            />
+            <div className="flex gap-2 items-center">
+              <i className="fa-regular fa-thumbs-up"></i>
+              <i className="fa-regular fa-thumbs-down"></i>
+            </div>
+          </div>
+          <div className=" flex flex-col w-5/6 h-5/6 border-black border-1 border-solid">
+            <h3 className="font-bold text-sm">Jamie</h3>
+            <p className="text-xs">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              nesciunt ipsum, voluptatibus odio labore libero!
+            </p>
+          </div>
+        </li>
+        <li className="border-black-50 border-solid border-2 flex gap-2 width-5/6 h-[80px]">
+          <div className="flex flex-col items-center w-1/6 gap-2">
+            <img
+              src="https://source.unsplash.com/random/?person/"
+              className="h-[50px] w-[50px] rounded"
+            />
+            <div className="flex gap-2 items-center">
+              <i className="fa-regular fa-thumbs-up"></i>
+              <i className="fa-regular fa-thumbs-down"></i>
+            </div>
+          </div>
+          <div className=" flex flex-col w-5/6 h-5/6 border-black border-1 border-solid">
+            <h3 className="font-bold text-sm">Jamie</h3>
+            <p className="text-xs">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              nesciunt ipsum, voluptatibus odio labore libero!
+            </p>
+          </div>
+        </li>
+        <li className="border-black-50 border-solid border-2 flex gap-2 width-5/6 h-[80px]">
+          <div className="flex flex-col items-center w-1/6 gap-2">
+            <img
+              src="https://source.unsplash.com/random/?person/"
+              className="h-[50px] w-[50px] rounded"
+            />
+            <div className="flex gap-2 items-center">
+              <i className="fa-regular fa-thumbs-up"></i>
+              <i className="fa-regular fa-thumbs-down"></i>
+            </div>
+          </div>
+          <div className=" flex flex-col w-5/6 h-5/6 border-black border-1 border-solid">
+            <h3 className="font-bold text-sm">Jamie</h3>
+            <p className="text-xs">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              nesciunt ipsum, voluptatibus odio labore libero!
+            </p>
+          </div>
+        </li>
+        <li className="border-black-50 border-solid border-2 flex gap-2 width-5/6 h-[80px]">
+          <div className="flex flex-col items-center w-1/6 gap-2">
+            <img
+              src="https://source.unsplash.com/random/?person/"
+              className="h-[50px] w-[50px] rounded"
+            />
+            <div className="flex gap-2 items-center">
+              <i className="fa-regular fa-thumbs-up"></i>
+              <i className="fa-regular fa-thumbs-down"></i>
+            </div>
+          </div>
+          <div className=" flex flex-col w-5/6 h-5/6 border-black border-1 border-solid">
+            <h3 className="font-bold text-sm">Jamie</h3>
+            <p className="text-xs">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              nesciunt ipsum, voluptatibus odio labore libero!
+            </p>
+          </div>
+        </li>
+        <li className="border-black-50 border-solid border-2 flex gap-2 width-5/6 h-[80px]">
+          <div className="flex flex-col items-center w-1/6 gap-2">
+            <img
+              src="https://source.unsplash.com/random/?person/"
+              className="h-[50px] w-[50px] rounded"
+            />
+            <div className="flex gap-2 items-center">
+              <i className="fa-regular fa-thumbs-up"></i>
+              <i className="fa-regular fa-thumbs-down"></i>
+            </div>
+          </div>
+          <div className=" flex flex-col w-5/6 h-5/6 border-black border-1 border-solid">
+            <h3 className="font-bold text-sm">Jamie</h3>
+            <p className="text-xs">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              nesciunt ipsum, voluptatibus odio labore libero!
+            </p>
+          </div>
+        </li>
+        <li className="border-black-50 border-solid border-2 flex gap-2 width-5/6 h-[80px]">
+          <div className="flex flex-col items-center w-1/6 gap-2">
+            <img
+              src="https://source.unsplash.com/random/?person/"
+              className="h-[50px] w-[50px] rounded"
+            />
+            <div className="flex gap-2 items-center">
+              <i className="fa-regular fa-thumbs-up"></i>
+              <i className="fa-regular fa-thumbs-down"></i>
+            </div>
+          </div>
+          <div className=" flex flex-col w-5/6 h-5/6 border-black border-1 border-solid">
+            <h3 className="font-bold text-sm">Jamie</h3>
+            <p className="text-xs">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              nesciunt ipsum, voluptatibus odio labore libero!
+            </p>
+          </div>
+        </li>
+        <li className="border-black-50 border-solid border-2 flex gap-2 width-5/6 h-[80px]">
+          <div className="flex flex-col items-center w-1/6 gap-2">
+            <img
+              src="https://source.unsplash.com/random/?person/"
+              className="h-[50px] w-[50px] rounded"
+            />
+            <div className="flex gap-2 items-center">
+              <i className="fa-regular fa-thumbs-up"></i>
+              <i className="fa-regular fa-thumbs-down"></i>
+            </div>
+          </div>
+          <div className=" flex flex-col w-5/6 h-5/6 border-black border-1 border-solid">
+            <h3 className="font-bold text-sm">Jamie</h3>
+            <p className="text-xs">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              nesciunt ipsum, voluptatibus odio labore libero!
+            </p>
+          </div>
+        </li>
+        <li className="border-black-50 border-solid border-2 flex gap-2 width-5/6 h-[80px]">
+          <div className="flex flex-col items-center w-1/6 gap-2">
+            <img
+              src="https://source.unsplash.com/random/?person/"
+              className="h-[50px] w-[50px] rounded"
+            />
+            <div className="flex gap-2 items-center">
+              <i className="fa-regular fa-thumbs-up"></i>
+              <i className="fa-regular fa-thumbs-down"></i>
+            </div>
+          </div>
+          <div className=" flex flex-col w-5/6 h-5/6 border-black border-1 border-solid">
+            <h3 className="font-bold text-sm">Jamie</h3>
+            <p className="text-xs">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              nesciunt ipsum, voluptatibus odio labore libero!
+            </p>
+          </div>
+        </li>
+        <li className="border-black-50 border-solid border-2 flex gap-2 width-5/6 h-[80px]">
+          <div className="flex flex-col items-center w-1/6 gap-2">
+            <img
+              src="https://source.unsplash.com/random/?person/"
+              className="h-[50px] w-[50px] rounded"
+            />
+            <div className="flex gap-2 items-center">
+              <i className="fa-regular fa-thumbs-up"></i>
+              <i className="fa-regular fa-thumbs-down"></i>
+            </div>
+          </div>
+          <div className=" flex flex-col w-5/6 h-5/6 border-black border-1 border-solid">
+            <h3 className="font-bold text-sm">Jamie</h3>
+            <p className="text-xs">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              nesciunt ipsum, voluptatibus odio labore libero!
+            </p>
+          </div>
+        </li>
+        <li className="border-black-50 border-solid border-2 flex gap-2 width-5/6 h-[80px]">
+          <div className="flex flex-col items-center w-1/6 gap-2">
+            <img
+              src="https://source.unsplash.com/random/?person/"
+              className="h-[50px] w-[50px] rounded"
+            />
+            <div className="flex gap-2 items-center">
+              <i className="fa-regular fa-thumbs-up"></i>
+              <i className="fa-regular fa-thumbs-down"></i>
+            </div>
+          </div>
+          <div className=" flex flex-col w-5/6 h-5/6 border-black border-1 border-solid">
+            <h3 className="font-bold text-sm">Jamie</h3>
+            <p className="text-xs">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              nesciunt ipsum, voluptatibus odio labore libero!
+            </p>
+          </div>
+        </li>
+        <li className="border-black-50 border-solid border-2 flex gap-2 width-5/6 h-[80px]">
+          <div className="flex flex-col items-center w-1/6 gap-2">
+            <img
+              src="https://source.unsplash.com/random/?person/"
+              className="h-[50px] w-[50px] rounded"
+            />
+            <div className="flex gap-2 items-center">
+              <i className="fa-regular fa-thumbs-up"></i>
+              <i className="fa-regular fa-thumbs-down"></i>
+            </div>
+          </div>
+          <div className=" flex flex-col w-5/6 h-5/6 border-black border-1 border-solid">
+            <h3 className="font-bold text-sm">Jamie</h3>
+            <p className="text-xs">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              nesciunt ipsum, voluptatibus odio labore libero!
+            </p>
+          </div>
+        </li>
+        <li className="border-black-50 border-solid border-2 flex gap-2 width-5/6 h-[80px]">
+          <div className="flex flex-col items-center w-1/6 gap-2">
+            <img
+              src="https://source.unsplash.com/random/?person/"
+              className="h-[50px] w-[50px] rounded"
+            />
+            <div className="flex gap-2 items-center">
+              <i className="fa-regular fa-thumbs-up"></i>
+              <i className="fa-regular fa-thumbs-down"></i>
+            </div>
+          </div>
+          <div className=" flex flex-col w-5/6 h-5/6 border-black border-1 border-solid">
+            <h3 className="font-bold text-sm">Jamie</h3>
+            <p className="text-xs">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              nesciunt ipsum, voluptatibus odio labore libero!
+            </p>
+          </div>
+        </li>
+        <li className="border-black-50 border-solid border-2 flex gap-2 width-5/6 h-[80px]">
+          <div className="flex flex-col items-center w-1/6 gap-2">
+            <img
+              src="https://source.unsplash.com/random/?person/"
+              className="h-[50px] w-[50px] rounded"
+            />
+            <div className="flex gap-2 items-center">
+              <i className="fa-regular fa-thumbs-up"></i>
+              <i className="fa-regular fa-thumbs-down"></i>
+            </div>
+          </div>
+          <div className=" flex flex-col w-5/6 h-5/6 border-black border-1 border-solid">
+            <h3 className="font-bold text-sm">Jamie</h3>
+            <p className="text-xs">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              nesciunt ipsum, voluptatibus odio labore libero!
+            </p>
+          </div>
+        </li>
+      </ul>
     </div>
   );
 };
