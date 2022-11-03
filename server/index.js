3feda715791c1d8608005753f41a55e04386f257
Brad Teague <b_rad88@live.com>
2022-11-03 10:59:50 +1030

    Implement JWT authentication

--- server/index.js
+++ server/index.js
@@ -1,19 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
 
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
-
+const { authMiddleware } = require("./utils/auth");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(require("cookie-parser")());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
