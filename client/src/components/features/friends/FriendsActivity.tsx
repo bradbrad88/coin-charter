81737c15785f13fb02fd21af19ee71867ee707d3
Ben Smerd <bsmerd@adam.com.au>
2022-11-03 19:31:00 +1030

    create friends component and friendsList component and FriendsActivity component

---
+++ client/src/components/features/friends/FriendsActivity.tsx
@@ -0,0 +1,9 @@
+const FriendsActivity = () => {
+  return (
+    <div className="flex flex-col rounded-sm gap-1 shadow-lg shadow-gray-400 m-4 p-5 w-[400px] h-screen">
+      <h1>Friends Activity</h1>
+    </div>
+  );
+};
+
+export default FriendsActivity;
---
+++ client/src/components/features/friends/FriendsList.tsx
@@ -0,0 +1,10 @@
+const FriendsList = () => {
+  //TODO SHOW PROFILE PIC, TITLE, NAME, AMOUNT FRIENDS, AND FAR RIGHT CAN SHOW MOST TOP RATED CHART THEY HAVE DONE
+  return (
+    <div className="flex rounded-sm gap-5 shadow-lg shadow-gray-400 p-5 m-5 w-[1050px] h-[400px]">
+      <h1>Friends List</h1>
+    </div>
+  );
+};
+
+export default FriendsList;
