851026194394ee354da20899a7004c7935baff34
Ben Smerd <bsmerd@adam.com.au>
2022-11-03 20:19:45 +1030

    added friends list and activity styling

--- client/src/components/features/friends/FriendsActivity.tsx
+++ client/src/components/features/friends/FriendsActivity.tsx
@@ -1,7 +1,7 @@
 const FriendsActivity = () => {
   return (
-    <div className="flex flex-col rounded-sm gap-1 shadow-lg shadow-gray-400 m-4 p-5 w-[400px] h-screen">
-      <h1>Friends Activity</h1>
+    <div className="flex flex-col rounded-sm gap-1 shadow-lg shadow-gray-400 m-4 p-5 w-2/6 h-[600px]">
+      <h1 className="text-xl font-bold">Friends Activity</h1>
     </div>
   );
 };
--- client/src/components/features/friends/FriendsList.tsx
+++ client/src/components/features/friends/FriendsList.tsx
@@ -1,8 +1,16 @@
 const FriendsList = () => {
   //TODO SHOW PROFILE PIC, TITLE, NAME, AMOUNT FRIENDS, AND FAR RIGHT CAN SHOW MOST TOP RATED CHART THEY HAVE DONE
   return (
-    <div className="flex rounded-sm gap-5 shadow-lg shadow-gray-400 p-5 m-5 w-[1050px] h-[400px]">
-      <h1>Friends List</h1>
+    <div className="flex flex-col rounded-sm shadow-lg shadow-gray-400 p-5 m-5 w-4/6 h-[600px]">
+      <div className="flex justify-between">
+        <h1 className="text-xl font-bold">Friends List</h1>
+        <h1>Friends: 2312</h1>
+      </div>
+      <ul className="flex flex-col gap-1 border-2 border-solid border-black-50 w-full h-full">
+        {/* //? MAKE THIS SPOT SUPPOSED TO BE THE MAP */}
+        {/* USERNAME, SUBTITLE, PROFILE PIC, BIO,  FRIENDS COUNT, NUM FAV COINS, NUM CHARTS MADE, NUM COMMENTS MADE*/}
+        <li className="border-solid border-2 border-black w-full h-[100px]"></li>
+      </ul>
     </div>
   );
 };
