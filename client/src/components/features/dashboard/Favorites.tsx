a16c1c3d086122efa46f0b3cefaff6ee88f1b922
Sammy <marchsam1988@gmail.com>
2022-11-03 21:40:58 +1030

    removed conflicting styling for mobile user

--- client/src/components/features/dashboard/Favorites.tsx
+++ client/src/components/features/dashboard/Favorites.tsx
@@ -1,6 +1,6 @@
 const Favorites = () => {
   return (
-    <div className="grid grid-cols-[1fr,_2fr] w-[500px] p-5 rounded-sm gap-5 shadow-lg shadow-gray-400 mt-20 ml-72 mr-72">
+    <div className=" p-5 rounded-sm gap-5 shadow-lg shadow-gray-400">
       <h1 className="text-center">Favorites</h1>
     </div>
   );
