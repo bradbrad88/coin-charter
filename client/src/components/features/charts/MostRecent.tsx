250f5792e735ad2a783f608dcb3689412ffa0fc5
Ben Smerd <bsmerd@adam.com.au>
2022-11-03 15:18:32 +1030

    created a chart for top rated and most recent based off the comments charts section

---
+++ client/src/components/features/charts/MostRecent.tsx
@@ -0,0 +1,86 @@
+const data = [
+  {
+    title: "This is what i see happening this month",
+    name: "Ben Smerd",
+    coin: "DogeCooin",
+    upVotes: 1202,
+    downVotes: 12,
+    createdAt: "24/12/2022",
+    chart: "https://source.unsplash.com/random/?person/",
+  },
+  {
+    title: "We are going UP!",
+    name: "Brad Teague",
+    coin: "Ethereum",
+    upVotes: 3203,
+    downVotes: 2,
+    createdAt: "21/03/2022",
+    chart: "https://source.unsplash.com/random/?person/",
+  },
+  {
+    title: "Going DOWN!",
+    name: "Sam March",
+    coin: "Avalanche",
+    upVotes: 1022,
+    downVotes: 1223,
+    createdAt: "24/12/2020",
+    chart: "https://source.unsplash.com/random/?person/",
+  },
+  {
+    title: "This i think will happen very soon in this market",
+    name: "Sally Peterson",
+    coin: "Theta",
+    upVotes: 1230,
+    downVotes: 122,
+    createdAt: "04/05/2022",
+    chart: "https://source.unsplash.com/random/?person/",
+  },
+  {
+    title: "This is what i see happening this month",
+    name: "Ben Smerdsssssssss",
+    coin: "DogeCooin",
+    upVotes: 1202,
+    downVotes: 12,
+    createdAt: "24/12/2022",
+    chart: "https://source.unsplash.com/random/?person/",
+  },
+];
+
+const MostRecent = () => {
+  return (
+    <div className="flex flex-col w-2/6 h-full pl-2">
+      <ul className="flex flex-col gap-1 h-[580px] w-full overflow-y-scroll">
+        {data.map((info) => (
+          <li className="border-2 border-solid border-black-50 flex flex-col">
+            <div className="flex justify-between">
+              <div className="flex flex-col w-5/6 h-[60px] ">
+                <h1 className="truncate font-bold text-md">{info.title}</h1>
+                <h1 className="truncate font-bold text-sm">{info.coin}</h1>
+                <div className="flex justify-between">
+                  <p className="truncate italic font-bold text-xs w-[110px]">
+                    By {info.name}
+                  </p>
+                  <p className="text-gray-500 text-xs break-all">
+                    Posted On: {info.createdAt}
+                  </p>
+                </div>
+              </div>
+
+              <div className="flex flex-col justify-start pr-2 pt-2">
+                <i className="fa-regular fa-thumbs-up text-[14px]"></i>
+                <p className="text-[8px]">{info.upVotes}</p>
+              </div>
+              <div className="flex flex-col justify-start pr-2 pt-2">
+                <i className="fa-regular fa-thumbs-down text-[14px]"></i>
+                <p className="text-[8px]">{info.downVotes}</p>
+              </div>
+            </div>
+            <img src={info.chart} className="w-full h-[120px]" />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MostRecent;
--- client/src/pages/Charts.tsx
+++ client/src/pages/Charts.tsx
@@ -1,5 +1,13 @@
+import TopRated from "../components/features/charts/TopRated";
+import MostRecent from "../components/features/charts/MostRecent";
+
 const Charts = () => {
-  return <div>Charts</div>;
+  return (
+    <div className="flex rounded-sm shadow-lg shadow-gray-400 p-5 m-5 w-[1050px] h-[600px]">
+      <TopRated />
+      <MostRecent />
+    </div>
+  );
 };
 
 export default Charts;
