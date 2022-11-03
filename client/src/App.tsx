90b8b4603e951a1bb2a6c879caf45faec38807be
Brad Teague <b_rad88@live.com>
2022-11-03 11:00:53 +1030

    Adjust jwt token storage to httpOnly cookie instead of local storage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,27 @@
 import { Routes, Route } from "react-router-dom";
-import Homepage from "pages/Homepage";
-import Friends from "pages/Friends";
-import Dashboard from "pages/Dashboard";
-import Charts from "pages/Charts";
 import Header from "common/Header";
+import Homepage from "pages/Homepage";
+import Dashboard from "pages/Dashboard";
+import Friends from "pages/Friends";
+import Charts from "pages/Charts";
 import CoinProfile from "pages/CoinProfile";
+import Login from "pages/Login";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
 } from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
+import Signup from "./pages/Signup";
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
   uri: "/graphql",
 });
 
-// Construct request middleware that will attach the JWT token to every request as an `authorization` header
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem("id_token");
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
-
 const client = new ApolloClient({
   // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-  link: authLink.concat(httpLink),
+  link: httpLink,
   cache: new InMemoryCache(),
 });
 
@@ -49,6 +37,8 @@ const App = () => {
           <Route path="charts" element={<Charts />} />
           {/* temporary in here while dev */}
           <Route path="coin/:coinId" element={<CoinProfile />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
         </Routes>
       </div>
     </ApolloProvider>
