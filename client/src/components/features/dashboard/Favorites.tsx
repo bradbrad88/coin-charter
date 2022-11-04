2545ed96e2facba903d9bd20b9fc75a9a79e4899
Sammy <marchsam1988@gmail.com>
2022-11-04 10:51:06 +1030

    favourites template started wip

--- client/src/components/features/dashboard/Favorites.tsx
+++ client/src/components/features/dashboard/Favorites.tsx
@@ -1,7 +1,18 @@
+import Comments from "./favoriteCards/Comments";
+import Charts from "./favoriteCards/Charts";
+import Coins from "./favoriteCards/Coins";
+
 const Favorites = () => {
   return (
     <div className=" p-5 rounded-sm gap-5 shadow-lg shadow-gray-400">
-      <h1 className="text-center">Favorites</h1>
+      <div>
+        <h1 className="text-center">Favorites</h1>
+      </div>
+      <div className="flex flex-row justify-between">
+        <Comments />
+        <Charts />
+        <Coins />
+      </div>
     </div>
   );
 };
