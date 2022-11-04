738bd2ef64a9b47080b5478cdc61186e4a060dea
Ben Smerd <bsmerd@adam.com.au>
2022-11-04 11:52:42 +1030

    updated container overflow

--- client/src/components/features/friends/FriendsList.tsx
+++ client/src/components/features/friends/FriendsList.tsx
@@ -48,14 +48,13 @@ const data = [
 ];
 
 const FriendsList = () => {
-  //TODO SHOW PROFILE PIC, TITLE, NAME, AMOUNT FRIENDS, AND FAR RIGHT CAN SHOW MOST TOP RATED CHART THEY HAVE DONE
   return (
     <div className="flex flex-col rounded-sm shadow-lg shadow-gray-400 p-5 m-5 w-4/6 h-[600px]">
       <div className="flex justify-between">
         <h1 className="text-xl font-bold">Friends List</h1>
         <h1>Friends: 2312</h1>
       </div>
-      <ul className="flex flex-col gap-1 border-2 border-solid border-black-50 w-full h-full">
+      <ul className="flex flex-col gap-1 w-full h-full overflow-y-auto">
         {data.map((info) => (
           <li className="border-b w-full h-[100px] flex justify-start p-1">
             <div className="flex gap-1 w-full">
@@ -69,7 +68,7 @@ const FriendsList = () => {
                     {info.name}
                   </h1>
                   <p className="italic text-sm">{info.subtitle}</p>
-                  <p className="text-sm w-full leading-4 overflow-y-scroll">
+                  <p className="text-sm w-full leading-4 overflow-y-auto">
                     {info.bio}
                   </p>
                 </div>
