4886b67025bc715bdc2fcb17fd8c10d0e7e4cb7b
Ben Smerd <bsmerd@adam.com.au>
2022-11-05 16:43:39 +1030

    created chart for coin by user

--- client/src/components/features/coinChart/MainChart.tsx
+++ client/src/components/features/coinChart/MainChart.tsx
@@ -1,8 +1,19 @@
 const MainChart = () => {
   return (
-    <div>
-      <h1>Chart for: Binance</h1>
-    </div>
+    <>
+      <div className="border-2 border-black border-solid w-4/6 flex flex-col">
+        <div className="flex justify-between items-center px-2">
+          <h1 className="text-lg font-bold">Chart for: Binance</h1>
+          <h1 className="text-lg font-bold">Created by: Ben Smerd</h1>
+          <h1 className="text-sm text-gray-500">Posted: 02/11/2021</h1>
+        </div>
+
+        <div className="border-solid border-2 border-black h-full"></div>
+      </div>
+      <div className="border-2 border-solid border-black w-2/6">
+        <h1 className="text-lg font-bold">Comments</h1>
+      </div>
+    </>
   );
 };
 
