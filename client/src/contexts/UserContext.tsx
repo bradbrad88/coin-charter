33914c6018ef84f7ca53361ea7fa17e426994685
Brad Teague <b_rad88@live.com>
2022-11-06 11:13:25 +1030

    Update user context to handle profile images

--- client/src/contexts/UserContext.tsx
+++ client/src/contexts/UserContext.tsx
@@ -1,20 +1,25 @@
 import { useEffect, useState } from "react";
-import createCtx from "./index";
 import { useMutation } from "@apollo/client";
-import { ADD_USER, LOGOUT_USER } from "src/graphql/queries";
+import { ADD_USER, LOGOUT_USER, ADD_PROFILE_IMAGE } from "src/graphql/queries";
+import createCtx from "./index";
+import useFetch from "hooks/useFetch";
+
+import type { Crop } from "react-image-crop";
+import type { Config } from "hooks/useFetch";
 
 interface Ctx {
   user: User | null;
+  isLoggedIn: boolean;
+  loading: boolean;
   loginUser: (user: User) => void;
   logoutUser: () => void;
   signUpUser: (newUser: NewUser) => void;
-  loading: boolean;
-  isLoggedIn: boolean;
+  updateImage: (image: File, crop: Crop) => void;
 }
 interface User {
   _id: string;
   username: string;
-  image: string;
+  image?: string;
 }
 
 interface Prototypes {
@@ -34,6 +39,8 @@ export const Provider = ({ children }: Prototypes) => {
   const [user, setUser] = useState<User | null>(localUser);
   const [addUser, { data: signupData, loading }] = useMutation(ADD_USER);
   const [logoutUserMutation] = useMutation(LOGOUT_USER);
+  const [addProfileImage, { data: imageData }] = useMutation(ADD_PROFILE_IMAGE);
+  const { postRequest } = useFetch();
 
   useEffect(() => {
     localStorage.setItem("user", JSON.stringify(user));
@@ -44,6 +51,17 @@ export const Provider = ({ children }: Prototypes) => {
       loginUser(signupData.addUser);
     }
   }, [signupData]);
+
+  useEffect(() => {
+    if (imageData)
+      setUser((prevState) => {
+        if (!prevState) return null;
+        return {
+          ...prevState,
+          image: imageData.addImage as string,
+        };
+      });
+  }, [imageData]);
 
   const loginUser = (user: User) => {
     setUser(user);
@@ -59,11 +77,34 @@ export const Provider = ({ children }: Prototypes) => {
     addUser({ variables: { ...newUser } });
   };
 
+  const updateImage = async (image: File, crop: Crop) => {
+    const data = new FormData();
+    data.append("image", image);
+    data.append("crop", JSON.stringify(crop));
+    const req: Config = {
+      data,
+      url: `/api/user/${user?._id}/upload-image`,
+    };
+    const res = await postRequest<{ url: string }>(req);
+    if (!res) return;
+
+    addProfileImage({ variables: { image: res.url } });
+    setUser((prevState: any) => ({ ...prevState, image: res.url }));
+  };
+
   const isLoggedIn = !!user;
 
   return (
     <UserProvider
-      value={{ user, loginUser, logoutUser, signUpUser, loading, isLoggedIn }}
+      value={{
+        user,
+        loginUser,
+        logoutUser,
+        signUpUser,
+        loading,
+        isLoggedIn,
+        updateImage,
+      }}
     >
       {children}
     </UserProvider>
