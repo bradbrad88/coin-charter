18ae5f24d672e1be9ab92cb9c663b4f14a07d764
Ben Smerd <bsmerd@adam.com.au>
2022-11-06 15:50:47 +1030

    charts responsiveness done

--- client/src/pages/Charts.tsx
+++ client/src/pages/Charts.tsx
@@ -4,12 +4,12 @@ import ChartsOfCoins from "../components/features/charts/ChartsOfCoins";
 
 const Charts = () => {
   return (
-    <div className="flex">
-      <div className="flex rounded-sm shadow-lg shadow-gray-400 p-5 m-5 w-[1050px] h-[600px]">
+    <div className="flex flex-col lg:flex-row">
+      <div className="flex rounded-sm shadow-lg shadow-gray-400 p-5 m-5 w-[95%] xl:w-[60%] h-[600px]">
         <TopRated />
         <MostRecent />
       </div>
-      <div className="flex flex-col rounded-sm gap-1 shadow-lg shadow-gray-400 m-4 p-5 w-[400px] h-screen">
+      <div className="flex flex-col rounded-sm gap-1 shadow-lg shadow-gray-400 m-4 p-5 w-[95%] lg:w-[35%] h-[600px]">
         <ChartsOfCoins />
       </div>
     </div>
