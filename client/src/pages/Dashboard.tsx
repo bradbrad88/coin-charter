156595159298c23fa48526fc9722cca11dbf2738
Brad Teague <b_rad88@live.com>
2022-11-06 22:37:03 +1030

    Dashboard component - get user data from context and pass to profile card

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,10 +1,23 @@
-import ProfileCard from "src/components/features/dashboard/ProfileCard";
-import Favorites from "src/components/features/dashboard/Favorites";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import useUserContext from "contexts/UserContext";
+import ProfileCard from "features/dashboard/ProfileCard";
+import Favorites from "features/dashboard/Favorites";
 
 const Dashboard = () => {
+  const { user, isLoggedIn } = useUserContext();
+  const nav = useNavigate();
+
+  // If the user is not logged in then navigate to main page
+  useEffect(() => {
+    if (!isLoggedIn) nav("/");
+  }, [isLoggedIn]);
+
+  if (!user) return null;
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 md:p-5 gap-5 max-w-screen-2xl mx-auto">
-      <ProfileCard />
+      <ProfileCard {...user} edit={true} />
       <Favorites />
     </div>
   );
