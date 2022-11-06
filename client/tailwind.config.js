5d12aef605f1951b82eb6599e6eab4184ef698ea
Brad Teague <b_rad88@live.com>
2022-11-06 12:16:17 +1030

    Set tailwind primary color class

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#5f5ac0",
+      },
+    },
   },
   plugins: [],
 };
