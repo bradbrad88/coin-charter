6f2561154f65c012f04ef22cff8a90b409ac52c2
Brad Teague <b_rad88@live.com>
2022-11-06 11:08:55 +1030

    Refactor auth middleware to be used with express router and apollo

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,31 +6,41 @@ if (!secret) throw new Error("JWT_SECRET missing from server .env file");
 
 const expiration = "2h";
 
-module.exports = {
-  authMiddleware: function ({ req, res }) {
-    let token =
-      req.body.token ||
-      req.query.token ||
-      req.headers.authorization ||
-      req.cookies.token;
+const authMiddleware = (req, res) => {
+  let token =
+    req.body.token ||
+    req.query.token ||
+    req.headers.authorization ||
+    req.cookies.token;
 
-    if (req.headers.authorization) {
-      token = token.split(" ").pop().trim();
-    }
+  if (req.headers.authorization) {
+    token = token.split(" ").pop().trim();
+  }
 
-    if (!token) {
-      return req;
-    }
-
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      res.clearCookie("token");
-      console.log("Invalid token");
-    }
-
+  if (!token) {
     return req;
+  }
+
+  try {
+    const { data } = jwt.verify(token, secret, { maxAge: expiration });
+    req.user = data;
+  } catch {
+    res.clearCookie("token");
+    console.log("Invalid token");
+  }
+
+  return req;
+};
+
+module.exports = {
+  gqlAuthMiddleware: function ({ req, res }) {
+    authMiddleware(req, res);
+    return req;
+  },
+  expressAuthMiddleware: function (req, res, next) {
+    authMiddleware(req, res);
+    if (!req.user) return res.sendStatus(401);
+    next();
   },
   signToken: function ({ _id }) {
     const payload = { _id };
