3e4451f232826c0832bf879f7f351ec99fff941f
Sammy <marchsam1988@gmail.com>
2022-11-07 16:01:06 +1030

    favorites and profile card changed to use container component

--- client/src/components/features/dashboard/ProfileCard.tsx
+++ client/src/components/features/dashboard/ProfileCard.tsx
@@ -5,6 +5,7 @@ import ImageEditor from "features/image-editor/ImageEditor";
 import Button from "common/Button";
 
 import type { ChangeEventHandler } from "react";
+import Container from "src/components/common/Container";
 
 interface Proptypes {
   _id: string;
@@ -49,63 +50,65 @@ const ProfileCard = ({
   };
 
   return (
-    <div className="grid grid-cols-1md:grid-cols-2 md:shadow-lg rounded-sm shadow-gray-400">
-      {editImage && (
-        <ImageEditor
-          image={editImage}
-          close={() => setEditImage(null)}
-          submit={submitImage}
-        />
-      )}
-      <div className="bg-gradient-to-b sm:bg-gradient from-indigo-800 md:from-white bg-opacity-80 flex justify-center p-5">
-        <div className="relative block w-1/2 md:w-[300px] h-fit">
-          <img
-            src={image}
-            className="aspect-[4/4] w-full object-cover rounded-full border-white border-4"
+    <Container>
+      <div className="grid grid-cols-1md:grid-cols-2">
+        {editImage && (
+          <ImageEditor
+            image={editImage}
+            close={() => setEditImage(null)}
+            submit={submitImage}
           />
-          {edit && (
-            <>
-              <label
-                htmlFor="avatar"
-                className="absolute flex justify-center items-center right-[15%] bottom-[15%] w-8 h-8 rounded-full bg-[rgb(95,90,192)] border-white border-2 translate-x-1/2 translate-y-1/2"
-              >
-                <MdEdit color="rgb(255,255,255)" />
-              </label>
-              <input
-                ref={imageRef}
-                className="hidden"
-                onInput={onImageEdit}
-                type="file"
-                id="avatar"
-                accept="image/jpeg, image/png"
-              />
-            </>
-          )}
+        )}
+        <div className="bg-gradient-to-b sm:bg-gradient from-indigo-800 md:from-white bg-opacity-80 flex justify-center p-5">
+          <div className="relative block w-1/2 md:w-[300px] h-fit">
+            <img
+              src={image}
+              className="aspect-[4/4] w-full object-cover rounded-full border-white border-4"
+            />
+            {edit && (
+              <>
+                <label
+                  htmlFor="avatar"
+                  className="absolute flex justify-center items-center right-[15%] bottom-[15%] w-8 h-8 rounded-full bg-[rgb(95,90,192)] border-white border-2 translate-x-1/2 translate-y-1/2"
+                >
+                  <MdEdit color="rgb(255,255,255)" />
+                </label>
+                <input
+                  ref={imageRef}
+                  className="hidden"
+                  onInput={onImageEdit}
+                  type="file"
+                  id="avatar"
+                  accept="image/jpeg, image/png"
+                />
+              </>
+            )}
+          </div>
+        </div>
+        <div className="p-5 grid gap-2">
+          <div className="flex justify-between gap-3">
+            <h1 className="text-2xl break-all">{username}</h1>
+            <h3 className="self-end text-slate-500">{subTitle}</h3>
+          </div>
+          <div className="flex justify-between gap-5">
+            {edit ? (
+              <Button onClick={() => {}}>Edit Profile</Button>
+            ) : (
+              <>
+                <Button onClick={() => {}}>Message</Button>
+                <Button onClick={() => {}}>Add Friend</Button>
+              </>
+            )}
+          </div>
+          <p className="italic">{bio}</p>
+          <hr />
+          <div className="flex justify-between gap-5 text-slate-500">
+            <p>Friends: {friendCount}</p>
+            <p>Posts: {postCount}</p>
+          </div>
         </div>
       </div>
-      <div className="p-5 grid gap-2">
-        <div className="flex justify-between gap-3">
-          <h1 className="text-2xl break-all">{username}</h1>
-          <h3 className="self-end text-slate-500">{subTitle}</h3>
-        </div>
-        <div className="flex justify-between gap-5">
-          {edit ? (
-            <Button onClick={() => {}}>Edit Profile</Button>
-          ) : (
-            <>
-              <Button onClick={() => {}}>Message</Button>
-              <Button onClick={() => {}}>Add Friend</Button>
-            </>
-          )}
-        </div>
-        <p className="italic">{bio}</p>
-        <hr />
-        <div className="flex justify-between gap-5 text-slate-500">
-          <p>Friends: {friendCount}</p>
-          <p>Posts: {postCount}</p>
-        </div>
-      </div>
-    </div>
+    </Container>
   );
 };
 
