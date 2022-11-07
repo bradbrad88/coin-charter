fa287646d5ff02638ad935d4c251800044ae3d7b
Ben Smerd <bsmerd@adam.com.au>
2022-11-07 14:47:51 +1030

    installed tradingview chart package and added in chart widget

--- client/src/components/common/Chart.tsx
+++ client/src/components/common/Chart.tsx
@@ -1,7 +1,13 @@
+import { AdvancedRealTimeChart } from "react-ts-tradingview-widgets";
+
 const Chart = () => {
   return (
-    <div>
-      <h1>CHart</h1>
+    <div className="flex flex-col rounded-sm shadow-lg shadow-gray-400 p-5 pb-7 m-5 w-[95%] lg:w-[97%] lg:h-[600px]">
+      <AdvancedRealTimeChart
+        theme="dark"
+        autosize
+        symbol="LINKUSDT"
+      ></AdvancedRealTimeChart>
     </div>
   );
 };
