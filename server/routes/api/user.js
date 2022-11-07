6937636d47cca788e293ee45b11a992213bf2ea4
Ben Smerd <bsmerd@adam.com.au>
2022-11-07 21:01:27 +1030

    added in chart upload file to AWS services

--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -2,6 +2,7 @@ const router = require("express").Router({ mergeParams: true });
 const multer = require("multer")().single("image");
 const upload = require("../../utils/s3");
 const resize = require("../../utils/resize-image");
+const { v4: uuidv4 } = require("uuid");
 
 router.post("/upload-image", multer, async (req, res) => {
   try {
@@ -13,6 +14,18 @@ router.post("/upload-image", multer, async (req, res) => {
     res.json({ url });
   } catch (error) {
     res.sendStatus(400);
+  }
+});
+
+router.post("/chart/:symbol", multer, async (req, res) => {
+  try {
+    const { userId, symbol } = req.params;
+    const image = await resize(req.file, crop);
+    const key = `${userId}${symbol}${uuidv4()}`;
+    const url = await upload(image, key);
+    res.json({ url });
+  } catch (error) {
+    res.status(400).send(error);
   }
 });
 
