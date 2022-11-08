e9450bef4de9f1fa8904060fd0dfdd14196ca22e
Brad Teague <b_rad88@live.com>
2022-11-08 19:54:33 +1030

    Implement search bar functionality for coins and users

--- client/src/components/common/Header.jsx
+++ client/src/components/common/Header.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 import NavBar from "./NavBar";
 import HamburgerButton from "./HamburgerButton";
 import { useState, useEffect } from "react";
-import SearchBar from "./SearchBar";
 import { TbArrowBackUp } from "react-icons/tb";
+import SearchBar from "features/search/SearchBar";
 import logo from "src/assets/logo.png";
 import invertLogo from "src/assets/logo small invert.png";
 
@@ -16,6 +16,7 @@ const Header = () => {
     window.addEventListener("resize", onResize);
     return () => window.removeEventListener("resize", onResize);
   }, []);
+
   return (
     <div className="flex lg:justify-between justify-center p-5 border-b relative gap-5">
       <div className="flex lg:hidden absolute left-0 top-7 pl-5">
@@ -29,7 +30,7 @@ const Header = () => {
       <div
         className={`${
           hideNav ? "" : "translate-x-full"
-        } fixed overflow-hidden top-0 bottom-0 left-[-100vw] right-[100vw] transition-transform bg-white w-full lg:relative lg:flex lg:translate-x-0 lg:transition-none lg:left-0 z-10`}
+        } fixed top-0 bottom-0 left-[-100vw] right-[100vw] transition-transform bg-white w-full lg:relative lg:flex lg:translate-x-0 lg:transition-none lg:left-0`}
       >
         <div className="flex lg:items-end flex-col lg:flex-row gap-3 bg-indigo-800 bg-opacity-80 p-6 h-full w-full lg:justify-between lg:p-0 lg:bg-white">
           <div className="absolute w-full left-0 top-3 flex justify-center lg:hidden">
@@ -44,7 +45,7 @@ const Header = () => {
             </button>
           </div>
           <div className="w-full lg:px-10">
-            <SearchBar />
+            <SearchBar closeSidebar={() => setHideNav(true)} />
           </div>
           <div className="flex flex-col lg:flex-row-reverse gap-5 lg:items-center">
             <NavBar close={() => setHideNav(true)} />
