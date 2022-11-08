a1ac8dedcbc1f4ceef5e9af33a8cb5526498f9c4
Sammy <marchsam1988@gmail.com>
2022-11-08 19:42:31 +1030

    polished navbar styling

--- client/src/components/common/NavItem.jsx
+++ client/src/components/common/NavItem.jsx
@@ -1,17 +1,27 @@
 import { Link, useLocation } from "react-router-dom";
 
-const NavItem = ({ title, path, onClick }) => {
+const NavItem = ({ title, path, onClick, disableActiveStyling }) => {
   const location = useLocation();
-  const active = location.pathname.split("/")[1] === path.replace("/", "");
+  const active =
+    location.pathname.split("/")[1] === path.replace("/", "") &&
+    !disableActiveStyling;
   return (
     <Link
-      className={`hover:translate-x-1 transition-all duration-300 ${
+      className={`transition-all duration-300 lg:hover:text-primary lg:hover:underline ${
         active ? "" : ""
       }`}
       to={path}
       onClick={onClick}
     >
-      <div className={`w-fit ${active ? "border-b" : ""}`}>{title}</div>
+      <div
+        className={`w-fit ${
+          active
+            ? "border-b lg:border-none lg:scale-110 lg:text-primary lg:font-medium"
+            : ""
+        }`}
+      >
+        {title}
+      </div>
     </Link>
   );
 };
