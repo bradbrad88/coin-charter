767b8c122066bb5d735f1f1ac5975307195380e4
Sammy <marchsam1988@gmail.com>
2022-11-10 21:37:39 +1030

    charts page has been made mobile friendly

--- client/src/components/features/charts/ChartsOfCoins.tsx
+++ client/src/components/features/charts/ChartsOfCoins.tsx
@@ -1,3 +1,7 @@
+import Container from "src/components/common/Container";
+import { IoIosArrowRoundUp } from "react-icons/Io";
+import { IoIosArrowRoundDown } from "react-icons/Io";
+
 const coinOption = ["Top Rated", "Most Recent", "Oldest", "Least Rated"];
 const data = [
   {
@@ -44,62 +48,64 @@ const data = [
 
 const ChartsOfCoins = () => {
   return (
-    <div className="flex flex-col w-[95%]">
-      <form className="mb-2 border-b pb-2">
-        <h1 className="font-bold">
-          View Charts For: <span className="italic">Bitcoin</span>
-        </h1>
-        <input
-          type="text"
-          value="Search Coin"
-          className="border-black-50 border-2"
-        />
-        <select>
-          {coinOption.map((option) => (
-            <option value={option} key={option}>
-              {option}
-            </option>
-          ))}
-        </select>
-      </form>
-      <ul className="flex flex-col gap-1 h-[500px] w-full overflow-y-scroll">
-        {data.map((info, index) => (
-          <li
-            className="group transition-all hover:bg-indigo-100 hover:rounded-lg hover:border-2 hover:border-indigo-100 hover:cursor-pointer flex flex-col p-2"
-            key={info.name + index}
-          >
-            <div className="flex justify-between">
-              <div className="flex flex-col w-5/6 h-[50px] gap-1">
-                <h1 className="truncate font-bold text-md text-indigo-600">
-                  {info.title}
-                </h1>
-                <div className="flex justify-between">
-                  <p className="truncate italic font-bold text-xs text-slate-500 w-[110px]">
-                    By {info.name}
-                  </p>
-                  <p className="text-gray-500 text-xs">
-                    Posted On: {info.createdAt}
-                  </p>
+    <Container>
+      <div className="flex flex-col">
+        <form className="p-5 sticky top-0 bg-white border-b border-t">
+          <h1 className="font-bold text-center ">
+            View Charts For:{" "}
+            <span className="italic font-semibold">Bitcoin</span>
+          </h1>
+          <div className="flex justify-center gap-5 p-5">
+            <input
+              type="text"
+              value="Search Coin"
+              className="border-black-50 border-2 rounded-full p-1 text-center"
+            />
+            <select>
+              {coinOption.map((option) => (
+                <option value={option} key={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+        </form>
+        <ul className="flex flex-col gap-1 h-full w-full overflow-y-scroll scrollbar hover:scrollbar-track-slate-200">
+          {data.map((info, index) => (
+            <li
+              className=" hover:bg-indigo-100 hover:border-indigo-100 hover:cursor-pointer flex flex-col p-2"
+              key={info.name + index}
+            >
+              <div className="flex justify-between">
+                <div className="flex flex-col w-5/6 h-[50px] gap-1">
+                  <h1 className="truncate font-bold text-md text-indigo-600">
+                    {info.title}
+                  </h1>
+                  <div className="flex justify-between">
+                    <p className="truncate italic font-bold text-xs text-slate-500 w-[110px]">
+                      By {info.name}
+                    </p>
+                    <p className="text-gray-500 text-xs">
+                      Posted On: {info.createdAt}
+                    </p>
+                  </div>
+                </div>
+
+                <div className="flex flex-col">
+                  <IoIosArrowRoundUp className="text-green-500" />
+                  <p className="text-[8px]">{info.upVotes}</p>
+                </div>
+                <div className="flex flex-col">
+                  <IoIosArrowRoundDown className="text-red-500" />
+                  <p className="text-[8px]">{info.downVotes}</p>
                 </div>
               </div>
-
-              <div className="flex flex-col justify-start pr-2 pt-2">
-                <i className="fa-regular fa-thumbs-up text-[14px]"></i>
-                <p className="text-[8px]">{info.upVotes}</p>
-              </div>
-              <div className="flex flex-col justify-start pr-2 pt-2">
-                <i className="fa-regular fa-thumbs-down text-[14px]"></i>
-                <p className="text-[8px]">{info.downVotes}</p>
-              </div>
-            </div>
-            <img
-              src={info.chart}
-              className="w-full h-[120px] group-hover:brightness-75 rounded-md"
-            />
-          </li>
-        ))}
-      </ul>
-    </div>
+              <img src={info.chart} className="w-full h-[120px] rounded-sm" />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Container>
   );
 };
 
