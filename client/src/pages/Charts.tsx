9b3529c85ad34f67f4c77fee17eadc2f30a190d3
Sammy <marchsam1988@gmail.com>
2022-11-11 18:20:34 +1030

    charts page responsive layout finished, waiting for data

--- client/src/pages/Charts.tsx
+++ client/src/pages/Charts.tsx
@@ -1,4 +1,3 @@
-import TopRated from "../components/features/charts/TopRated";
 import MostRecent from "../components/features/charts/MostRecent";
 import ChartsOfCoins from "../components/features/charts/ChartsOfCoins";
 
@@ -6,7 +5,6 @@ const Charts = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 md:p-5 gap-5 max-w-screen-2xl mx-auto">
       <div className="space-y-5">
-        <TopRated />
         <MostRecent />
       </div>
       <div className="">
