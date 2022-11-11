e668f09d52d68edc46b76c03b50299ace42af458
Brad Teague <b_rad88@live.com>
2022-11-11 19:09:14 +1030

    Adjust seed data to add coins and friends

--- server/utils/seed.js
+++ server/utils/seed.js
@@ -1,23 +1,52 @@
 // Import connection, models and seed data
 const connection = require("../config/connection");
-const { Users } = require("../models");
-const { userSeed } = require("./data");
+const { Users, Coins } = require("../models");
+const { userSeed, coinSeed } = require("./data");
 
 connection.on("error", (err) => err);
 
 // Create connection and load seed data into the models
 connection.once("open", async () => {
   console.log("Connected");
+  await Coins.deleteMany({});
+
+  const coinPromises = coinSeed.map(async (coin) => {
+    return await Coins.create(coin);
+  });
+
+  const coins = await Promise.all(coinPromises);
 
   // Drop any existing documents from models
   await Users.deleteMany({});
 
   // Insert new dataseed into models
+
   const userPromises = userSeed.map(async (user) => {
-    await Users.create(user);
+    return await Users.create({
+      ...user,
+      favCoins: [{ coin: coins[Math.floor(Math.random() * coins.length)] }],
+    });
   });
 
-  await Promise.all(userPromises);
+  const users = await Promise.all(userPromises);
+
+  const friendPromises = users.map(async (user, idx) => {
+    const id = users[(idx + 1) % users.length]._id;
+    return await user.updateOne(
+      {
+        $push: {
+          friends: {
+            $each: [{ friend: id }],
+            $position: 0,
+          },
+        },
+      },
+      { timestamps: true, new: true },
+    );
+  });
+
+  const newUsers = await Promise.all(friendPromises);
 
   console.log("Seed complete");
+  process.exit();
 });
