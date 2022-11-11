1fb748646eaaa733e2a855b9fc30897c1f488c9c
Ben Smerd <bsmerd@adam.com.au>
2022-11-11 17:25:42 +1030

    updated functionality to the coinProfile page with charts based on criteria selected by the user

--- client/src/components/features/coinProfile/CoinCharts.tsx
+++ client/src/components/features/coinProfile/CoinCharts.tsx
@@ -1,7 +1,13 @@
 import TopCharts from "./TopCharts";
 import OtherCharts from "./OtherCharts";
+import { CoinType } from "src/pages/CoinProfile";
 
-const CoinCharts = () => {
+interface PropTypes {
+  coin: CoinType;
+}
+
+const CoinCharts = ({ coin }: PropTypes) => {
+  const coinId = coin.id;
   return (
     <div className="flex flex-col rounded-sm shadow-lg shadow-gray-400 p-5 m-5 w-[95%] lg:w-[97%] lg:h-[600px]">
       <div className="flex gap-3">
@@ -9,8 +15,8 @@ const CoinCharts = () => {
         <i className="fa-solid fa-chart-line text-lg"></i>
       </div>
       <div className="flex">
-        <TopCharts />
-        <OtherCharts />
+        <TopCharts coinId={coinId} />
+        <OtherCharts coinId={coinId} />
       </div>
     </div>
   );
