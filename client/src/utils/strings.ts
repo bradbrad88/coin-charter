4bc324fef9b43f5781f36b2c1a37f0d6eb71cec9
Ben Smerd <bsmerd@adam.com.au>
2022-11-11 20:37:03 +1030

    added in functionality for database querying for the charts page where users can view criteria for charts based on a coins

--- client/src/utils/strings.ts
+++ client/src/utils/strings.ts
@@ -8,3 +8,30 @@ export const formatCurrency = (
 export const formatPercentage = (number: string | number, decimals = 2) => {
   return parseFloat(number.toString()).toFixed(decimals) + "%";
 };
+
+// toprated info
+const nav = useNavigate();
+const [topRated, setTopRated] = useState<any>();
+const { loading, error, data } = useQuery(QUERY_ALL_CHARTS);
+
+useEffect(() => {
+  const info = data?.charts;
+  if (info) {
+    topRatedChart(info);
+  }
+}, [data]);
+
+const topRatedChart = (info: any) => {
+  const mostUpvotedChart = info.reduce(function (prev: any, current: any) {
+    return prev.upVotes > current.upVotes ? prev : current;
+  });
+  setTopRated(mostUpvotedChart);
+};
+
+if (!topRated) {
+  return <div>Loading...</div>;
+}
+
+const selectUser = () => {
+  nav(`/profile/${topRated.userId}`);
+};
