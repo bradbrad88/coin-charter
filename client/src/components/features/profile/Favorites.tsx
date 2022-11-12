3aa7eb1c17c79c919fc569547efad593c8d6fd8c
Brad Teague <b_rad88@live.com>
2022-11-12 11:30:18 +1030

    Add friend requests to profile favourites

--- client/src/components/features/profile/Favorites.tsx
+++ client/src/components/features/profile/Favorites.tsx
@@ -1,13 +1,13 @@
-import Comments from "./favoriteCards/Comments";
 import Charts from "./favoriteCards/Charts";
 import Coins from "./favoriteCards/Coins";
 import Tabs from "common/Tabs";
 import Container from "src/components/common/Container";
+import FriendRequests from "../friends/FriendRequests";
 
 const componentList = [
   { title: "Coins", component: <Coins /> },
-  { title: "Comments", component: <Comments /> },
   { title: "Charts", component: <Charts /> },
+  { title: "Friend Requests", component: <FriendRequests /> },
 ];
 
 const Favorites = () => {
