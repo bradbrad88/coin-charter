9d83991ff7df3ea49305313508768cef5416eb17
Ben Smerd <bsmerd@adam.com.au>
2022-11-12 14:47:39 +1030

    fixed up merge conflicts with chart functionality to database and API with the responsiveness of pages

--- client/src/pages/Charts.tsx
+++ client/src/pages/Charts.tsx
@@ -1,11 +1,11 @@
-import MostRecent from "../components/features/charts/MostRecent";
+import TopCharts from "../components/features/charts/TopCharts";
 import ChartsOfCoins from "../components/features/charts/ChartsOfCoins";
 
 const Charts = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 md:p-5 gap-5 max-w-screen-2xl mx-auto">
       <div className="space-y-5">
-        <MostRecent />
+        <TopCharts />
       </div>
       <div className="">
         <ChartsOfCoins />
