48693e68f72861fdcba3f1375f7cb022550ce028
Brad Teague <b_rad88@live.com>
2022-11-13 00:48:10 +1030

    Refactor chart coin component with global types

--- client/src/pages/ChartCoin.tsx
+++ client/src/pages/ChartCoin.tsx
@@ -1,53 +1,23 @@
-import MainChart from "src/components/features/coinChart/MainChart";
-import Comments from "src/components/features/coinChart/Comments";
-import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { QUERY_CHART } from "../graphql/queries";
-
-interface ChartId {
-  chartId: string;
-}
-
-interface ChartInfoType {
-  _id: string;
-  coinId: string;
-  coinName: string;
-  symbol: string;
-  chartTitle: string;
-  chartDescription: string;
-  username: string;
-  imageThumbnail: string;
-  imageMedium: string;
-  imageSmall: string;
-  chartComments: {
-    _id: string;
-    commentText: string;
-    createdAt: number;
-  };
-  upVotes: number;
-  downVotes: number;
-  createdAt: number;
-}
+import MainChart from "src/components/features/coinChart/MainChart";
+import Comments from "src/components/features/coinChart/Comments";
 
 const ChartCoin = () => {
   const { chartId } = useParams();
-  const [chartInfo, setChartInfo] = useState<ChartInfoType>();
-  const { loading, error, data } = useQuery(QUERY_CHART, {
+
+  const { data } = useQuery<{ chart: Chart }>(QUERY_CHART, {
     variables: { chartId },
   });
 
-  useEffect(() => {
-    let chartData = data?.chart;
-    if (chartData) {
-      setChartInfo(chartData);
-    }
-  }, [data]);
+  const chart = data?.chart;
+
   return (
     <div>
       <div className="flex flex-col md:flex-row gap-4 rounded-sm shadow-lg shadow-gray-400 p-5 m-5 w-[95%] md:h-[600px]">
-        <MainChart chartInfo={chartInfo} />
-        <Comments chartInfo={chartInfo} />
+        {chart && <MainChart chart={chart} />}
+        <Comments chartInfo={chart} />
       </div>
     </div>
   );
