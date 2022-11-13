24bfe7018d4ed86269b7016025beece9bb759f5b
Ben Smerd <bsmerd@adam.com.au>
2022-11-13 19:47:52 +1030

    added comment functionality on the chart for coins and the coin page so users can comment and view their comments

--- client/src/components/features/coinChart/Comments.tsx
+++ client/src/components/features/coinChart/Comments.tsx
@@ -1,97 +1,24 @@
-const hardcode = [
-  {
-    name: "Ben",
-    comment:
-      "Great charting there, i really liked how you analysed everything so well it looks really good and i agree with everything ou said here and it hink it will happen exactly like the way you said it willthere, i really liked how you analysed everything so well it looks really good and i agree with everything ou said here and it hink it will happen exactly like the way you said it will",
-    upVotes: 1290,
-    downVotes: 13,
-    profilePic: "https://source.unsplash.com/random/?person/",
-  },
-  {
-    name: "Brad",
-    comment: "I LOVE THIS COIN SO MUCH",
-    upVotes: 390,
-    downVotes: 1123,
-    profilePic: "https://source.unsplash.com/random/?person/",
-  },
-  {
-    name: "Same",
-    comment: "I am in love with this and and so good",
-    upVotes: 3910,
-    downVotes: 1332,
-    profilePic: "https://source.unsplash.com/random/?person/",
-  },
-  {
-    name: "Jamie",
-    comment:
-      "I rasd love it so much sooooo much THE BESTTTeally enjoy this coin because it is so good and i love it so much because it is so good and so good",
-    upVotes: 390,
-    downVotes: 13,
-    profilePic: "https://source.unsplash.com/random/?person/",
-  },
-  {
-    name: "Same",
-    comment: "I am in love with this and and so good",
-    upVotes: 3910,
-    downVotes: 1332,
-    profilePic: "https://source.unsplash.com/random/?person/",
-  },
-  {
-    name: "Jamie",
-    comment:
-      "I rasd love it so much sooooo much THE BESTTTeally enjoy this coin because it is so good and i love it so much because it is so good and so good",
-    upVotes: 390,
-    downVotes: 13,
-    profilePic: "https://source.unsplash.com/random/?person/",
-  },
-  {
-    name: "Same",
-    comment: "I am in love with this and and so good",
-    upVotes: 3910,
-    downVotes: 1332,
-    profilePic: "https://source.unsplash.com/random/?person/",
-  },
-  {
-    name: "Jamie",
-    comment:
-      "I rasd love it so much sooooo much THE BESTTTeally enjoy this coin because it is so good and i love it so much because it is so good and so good",
-    upVotes: 390,
-    downVotes: 13,
-    profilePic: "https://source.unsplash.com/random/?person/",
-  },
-  {
-    name: "Same",
-    comment: "I am in love with this and and so good",
-    upVotes: 3910,
-    downVotes: 1332,
-    profilePic: "https://source.unsplash.com/random/?person/",
-  },
-  {
-    name: "Jamie",
-    comment:
-      "I rasd love it so much sooooo much THE BESTTTeally enjoy this coin because it is so good and i love it so much because it is so good and so good",
-    upVotes: 390,
-    downVotes: 13,
-    profilePic: "https://source.unsplash.com/random/?person/",
-  },
-];
-
 import { useState, useEffect } from "react";
-import { QUERY_CHART, ADD_CHART_COMMENT } from "src/graphql/queries";
+import { QUERY_CHART_COMMENTS, ADD_CHART_COMMENT } from "src/graphql/queries";
 import { useQuery, useMutation } from "@apollo/client";
 import Button from "src/components/common/Button";
+import { Link } from "react-router-dom";
 
-const CreateComment = ({ chartId }: any) => {
+interface Proptypes {
+  chart: Chart;
+}
+
+const CreateComment = ({ chart }: Proptypes) => {
+  const chartId = chart?._id;
   const [commentInput, setCommentInput] = useState<string>("");
   const [addChartComment, { error, data }] = useMutation(ADD_CHART_COMMENT);
-
-  console.log(chartId);
 
   const submitComment = () => {
     const variables = {
       commentText: commentInput,
       chartId,
     };
+
     addChartComment({ variables });
   };
 
@@ -116,21 +43,27 @@ const CreateComment = ({ chartId }: any) => {
   );
 };
 
-const Comments = (chartInfo: any) => {
-  const chartId = chartInfo?._id;
+const Comments = ({ chart }: Proptypes) => {
+  const chartId = chart?._id;
   const [commentsList, setCommentsList] = useState<any>();
-  const { loading, error, data } = useQuery(QUERY_CHART, {
-    variables: { chartId },
-  });
-
+  const { loading, error, data } = useQuery<{ chartComments: Chart }>(
+    QUERY_CHART_COMMENTS,
+    {
+      variables: { chartId },
+    },
+  );
   useEffect(() => {
-    let chartData = data?.chart;
+    let chartData = data?.chartComments;
+
     if (chartData) {
-      console.log(chartData);
+      setCommentsList(chartData);
     }
   }, [data]);
 
-  console.log(chartId);
+  if (!commentsList) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="w-full md:w-2/6 flex flex-col">
       <div className="flex gap-2">
@@ -139,35 +72,37 @@ const Comments = (chartInfo: any) => {
       </div>
 
       <ul className="flex flex-col gap-1 overflow-y-scroll">
-        {hardcode.map((info, index) => (
+        {commentsList.map((info: any, index: number) => (
           <li
-            key={info.name + index}
+            key={info.username + index}
             className="border-black-50 border-solid border-2 flex gap-2 width-5/6 h-[92px]"
           >
             <div className="flex flex-col items-center w-1/6 gap-1 p-1">
-              <img
-                src={info.profilePic}
-                className="h-[50px] w-[50px] rounded"
-              />
+              <img src={info.image} className="h-[50px] w-[50px] rounded" />
               <div className="flex gap-2 items-center">
                 <div className="flex flex-col">
                   <i className="fa-regular fa-thumbs-up text-[14px]"></i>
-                  <p className="text-[8px]">{info.upVotes}</p>
+                  <p className="text-[8px]">{info.upVotes.length}</p>
                 </div>
                 <div className="flex flex-col">
                   <i className="fa-regular fa-thumbs-down text-[14px]"></i>
-                  <p className="text-[8px]">{info.downVotes}</p>
+                  <p className="text-[8px]">{info.downVotes.length}</p>
                 </div>
               </div>
             </div>
             <div className=" flex flex-col w-5/6 h-5/6">
-              <h3 className="font-bold text-sm">{info.name}</h3>
-              <p className="text-xs overflow-y-auto">{info.comment}</p>
+              <Link to={`/profile/${info.userId}`}>
+                <span className="font-bold text-sm text-indigo-600">
+                  {info.username}
+                </span>
+              </Link>
+              <p className="text-gray-500 text-xs">{info.createdAt}</p>
+              <p className="text-xs overflow-y-auto">{info.commentText}</p>
             </div>
           </li>
         ))}
       </ul>
-      <CreateComment chartId={chartId} />
+      <CreateComment chart={chart} />
     </div>
   );
 };
