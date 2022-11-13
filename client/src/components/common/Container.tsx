587e747f3e4f615654dce9a3f49b5d70fc697d6f
Sammy <marchsam1988@gmail.com>
2022-11-13 20:37:27 +1030

    charts page responsive fix

--- client/src/components/common/Container.tsx
+++ client/src/components/common/Container.tsx
@@ -4,7 +4,9 @@ interface Proptypes {
 
 const Container = ({ children }: Proptypes) => {
   return (
-    <div className="md:shadow-2xl rounded-sm shadow-gray-900">{children}</div>
+    <div className="md:shadow-2xl rounded-sm shadow-gray-900 h-full">
+      {children}
+    </div>
   );
 };
 
