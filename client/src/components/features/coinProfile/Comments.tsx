2d89f9c340b4db8dee4e14a5779365544ddca19d
Ben Smerd <bsmerd@adam.com.au>
2022-11-14 15:35:43 +1030

    added create comment styling and changed display when no comments in database

--- client/src/components/features/coinProfile/Comments.tsx
+++ client/src/components/features/coinProfile/Comments.tsx
@@ -31,7 +31,7 @@ const CreateComment = ({ coinName, coinId }: CoinInfo) => {
       <h1>Add Comment</h1>
       <form>
         <textarea
-          className="w-full text-sm"
+          className="w-full text-sm border border-solid border-gray-300 rounded p-1"
           placeholder="Add text here..."
           rows={3}
           value={commentInput}
@@ -66,40 +66,47 @@ const Comments = ({ coinName, coinId }: CoinInfo) => {
 
   return (
     <>
-      <ul className="flex flex-col gap-1 overflow-y-scroll h-4/6">
-        {commentsList.map((info: any, index: number) => (
-          <li
-            key={info.username + index}
-            className="border-black-50 border-solid border-2 flex gap-2 width-5/6 h-[85px]"
-          >
-            <div className="flex flex-col items-center w-1/6 gap-1 p-1">
-              <img src={info.image} className="h-[50px] w-[50px] rounded" />
-              <div className="flex gap-2 items-center">
-                <div className="flex flex-col">
-                  <i className="fa-regular fa-thumbs-up text-[14px]"></i>
-                  <p className="text-[8px]">{info.upVotes}</p>
-                </div>
-                <div className="flex flex-col">
-                  <i className="fa-regular fa-thumbs-down text-[14px]"></i>
-                  <p className="text-[8px]">{info.downVotes}</p>
+      {commentsList.length === 0 ? (
+        <div className="h-4/6">
+          No comments made yet, would you like to be the first?
+        </div>
+      ) : (
+        <ul className="flex flex-col gap-1 overflow-y-scroll h-4/6">
+          {commentsList.map((info: any, index: number) => (
+            <li
+              key={info.username + index}
+              className="border-black-50 border-solid border-2 flex gap-2 width-5/6 h-[85px]"
+            >
+              <div className="flex flex-col items-center w-1/6 gap-1 p-1">
+                <img src={info.image} className="h-[50px] w-[50px] rounded" />
+                <div className="flex gap-2 items-center">
+                  <div className="flex flex-col">
+                    <i className="fa-regular fa-thumbs-up text-[14px]"></i>
+                    <p className="text-[8px]">{info.upVotes}</p>
+                  </div>
+                  <div className="flex flex-col">
+                    <i className="fa-regular fa-thumbs-down text-[14px]"></i>
+                    <p className="text-[8px]">{info.downVotes}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className=" flex flex-col w-5/6 h-5/6">
-              <div className="flex gap-2 items-center">
-                <Link to={`/profile/${info.userId}`}>
-                  <span className="font-bold text-sm text-indigo-600">
-                    {info.username}
-                  </span>
-                </Link>
-                <p className="text-gray-500 text-xs">{info.createdAt}</p>
-              </div>
+              <div className=" flex flex-col w-5/6 h-5/6">
+                <div className="flex gap-2 items-center">
+                  <Link to={`/profile/${info.userId}`}>
+                    <span className="font-bold text-sm text-indigo-600">
+                      {info.username}
+                    </span>
+                  </Link>
+                  <p className="text-gray-500 text-xs">{info.createdAt}</p>
+                </div>
 
-              <p className="text-xs overflow-y-auto">{info.commentText}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
+                <p className="text-xs overflow-y-auto">{info.commentText}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+
       <CreateComment coinId={coinId} coinName={coinName} />
     </>
   );
