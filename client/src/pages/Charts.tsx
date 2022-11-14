469c71dae646733f0a5da68f9dfd35ca43631970
Sammy <marchsam1988@gmail.com>
2022-11-14 13:08:41 +1030

    finished styling and used new component

--- client/src/pages/Charts.tsx
+++ client/src/pages/Charts.tsx
@@ -3,8 +3,8 @@ import ChartsOfCoins from "../components/features/charts/ChartsOfCoins";
 
 const Charts = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 md:p-5 gap-5 max-w-screen-2xl mx-auto max-h-screen">
-      <div className="col-span-2">
+    <div className="grid grid-cols-1 lg:grid-cols-3 md:p-5 gap-5 max-w-screen-2xl mx-auto max-h-screen w-screen">
+      <div className="lg:col-span-2">
         <TopCharts />
       </div>
 
