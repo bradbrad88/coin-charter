e56d25cd326e59f5d87f0e130ddacd2e852217cb
Sammy <marchsam1988@gmail.com>
2022-11-14 19:13:32 +1030

    responsive layout for charts page, comments and individual charts finished

--- client/src/components/features/coinChart/Comments.tsx
+++ client/src/components/features/coinChart/Comments.tsx
@@ -3,6 +3,8 @@ import { QUERY_CHART_COMMENTS, ADD_CHART_COMMENT } from "src/graphql/queries";
 import { useQuery, useMutation } from "@apollo/client";
 import Button from "src/components/common/Button";
 import { Link } from "react-router-dom";
+import Container from "src/components/common/Container";
+import VoteWidget from "src/components/common/VoteWidget";
 
 interface Proptypes {
   chart: Chart;
@@ -27,11 +29,11 @@ const CreateComment = ({ chart }: Proptypes) => {
   }
 
   return (
-    <div>
-      <h1>Add Comment</h1>
+    <div className="p-5">
+      <h1 className="text-center">Add Comment:</h1>
       <form>
         <textarea
-          className="w-full text-sm"
+          className="w-full text-sm bg-slate-200"
           placeholder="Add text here..."
           rows={3}
           value={commentInput}
@@ -65,45 +67,49 @@ const Comments = ({ chart }: Proptypes) => {
   }
 
   return (
-    <div className="w-full md:w-2/6 flex flex-col">
-      <div className="flex gap-2">
-        <h1 className="text-lg font-bold">Comments</h1>
-        <i className="fa-regular fa-comments text-lg"></i>
-      </div>
-
-      <ul className="flex flex-col gap-1 overflow-y-scroll">
-        {commentsList.map((info: any, index: number) => (
-          <li
-            key={info.username + index}
-            className="border-black-50 border-solid border-2 flex gap-2 width-5/6 h-[92px]"
-          >
-            <div className="flex flex-col items-center w-1/6 gap-1 p-1">
-              <img src={info.image} className="h-[50px] w-[50px] rounded" />
-              <div className="flex gap-2 items-center">
-                <div className="flex flex-col">
-                  <i className="fa-regular fa-thumbs-up text-[14px]"></i>
-                  <p className="text-[8px]">{info.upVotes.length}</p>
+    <Container>
+      <div className="flex flex-col">
+        <h1 className="text-lg font-bold text-center">Comments</h1>
+        <CreateComment chart={chart} />
+        <ul>
+          {commentsList.map((info: any, index: number) => (
+            <li
+              key={info.username + index}
+              className="border-black-50 border-solid border-2 flex flex-row justify-between h-[92px]"
+            >
+              <div className="flex">
+                <div className="">
+                  <img src={info.image} className="h-[50px] w-[50px] rounded" />
+                  <div className="">
+                    <Link to={`/profile/${info.userId}`}>
+                      <span className="font-bold text-sm text-indigo-600">
+                        {info.username}
+                      </span>
+                    </Link>
+                  </div>
                 </div>
-                <div className="flex flex-col">
-                  <i className="fa-regular fa-thumbs-down text-[14px]"></i>
-                  <p className="text-[8px]">{info.downVotes.length}</p>
+                <div>
+                  <p className="text-xs p-5">{info.commentText}</p>
                 </div>
               </div>
-            </div>
-            <div className=" flex flex-col w-5/6 h-5/6">
-              <Link to={`/profile/${info.userId}`}>
-                <span className="font-bold text-sm text-indigo-600">
-                  {info.username}
-                </span>
-              </Link>
-              <p className="text-gray-500 text-xs">{info.createdAt}</p>
-              <p className="text-xs overflow-y-auto">{info.commentText}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-      <CreateComment chart={chart} />
-    </div>
+              <div className="flex flex-col justify-between p-1">
+                <VoteWidget
+                  handleUpVote={(vote) => {}}
+                  handleDownVote={(vote) => {}}
+                  downVote={true}
+                  upVote={true}
+                  downVoteCount={1}
+                  upVoteCount={2}
+                />
+                <p className="text-gray-500 text-xs ml-auto">
+                  {info.createdAt}
+                </p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Container>
   );
 };
 
--- client/src/components/features/coinChart/MainChart.tsx
+++ client/src/components/features/coinChart/MainChart.tsx
@@ -13,49 +13,55 @@ const MainChart = ({ chart }: Proptypes) => {
 
   return (
     <Container>
-      <div className="md:h-[600px] w-full md:w-4/6 flex flex-col">
-        <div className="flex flex-col">
-          <div className="flex justify-between items-center">
-            <h1 className="text-lg font-bold">
-              Chart for:{" "}
-              <Link
-                to={`/coin/${chart.coinId}`}
-                className="hover:text-indigo-600"
-              >
-                <span className="text-indigo-600 hover:cursor-pointer leading-none">
-                  {chart.coinName}
-                </span>
-              </Link>
-            </h1>
-            <h1 className="text-lg font-bold">
-              Created by:{" "}
-              <Link to={`/profile/${chart.userId}`}>
-                <span className="text-indigo-600 hover:cursor-pointer">
-                  {chart.username}
-                </span>
-              </Link>
-            </h1>
-            <h1 className="text-sm text-gray-500">Posted: {chart.createdAt}</h1>
-            <div className="flex gap-5 pl-2">
-              <VoteWidget
-                handleUpVote={(vote) => {}}
-                handleDownVote={(vote) => {}}
-                downVote={true}
-                upVote={true}
-                downVoteCount={1}
-                upVoteCount={2}
-              />
+      <div className="p-5 flex flex-col gap-5">
+        <div className="md:flex flex-col">
+          <div className="flex justify-between">
+            <div className="lg:flex lg:gap-5 gap-1 mt-auto lg:p-3">
+              <h1 className="text-md font-semibold">
+                Coin:{" "}
+                <Link to={`/coin/${chart.coinId}`}>
+                  <span className="text-indigo-600 hover:cursor-pointer hover:underline leading-none uppercase text-lg">
+                    {chart.coinName}
+                  </span>
+                </Link>
+              </h1>
+              <h1 className="text-sm font-semibold">
+                Created by:{" "}
+                <Link to={`/profile/${chart.userId}`}>
+                  <span className="text-indigo-600 hover:cursor-pointer hover:underline italic">
+                    {chart.username}
+                  </span>
+                </Link>
+              </h1>
+            </div>
+            <p className="lg:text-lg font-semibold text-black capitalize">
+              {chart.chartTitle}
+            </p>
+            <div className="flex flex-col lg:flex-row lg:gap-5 lg:p-3">
+              <div className="flex lg:gap-5 lg:pl-2">
+                <VoteWidget
+                  handleUpVote={(vote) => {}}
+                  handleDownVote={(vote) => {}}
+                  downVote={true}
+                  upVote={true}
+                  downVoteCount={1}
+                  upVoteCount={2}
+                />
+              </div>
+              <h1 className="text-sm text-gray-500 mt-auto">
+                Posted: {chart.createdAt}
+              </h1>
             </div>
           </div>
-          <p className="text-lg font-bold text-indigo-600">
-            {chart.chartTitle}
-          </p>
-          <p className="text-sm max-h-16 overflow-y-scroll">
+          <img className="h-full md:h-full" src={chart.imageThumbnail} />
+        </div>
+        <div>
+          <p className="text-md max-h-16 truncate">
+            {" "}
+            <span className="font-semibold">Chart Info:</span> <br></br>
             {chart.chartDescription}
           </p>
         </div>
-
-        <img className=" h-3/6 md:h-4/6" src={chart.imageThumbnail} />
       </div>
     </Container>
   );
