ce946a616fb79317472b39457973a2f4273abc6f
Sammy <marchsam1988@gmail.com>
2022-11-14 13:07:46 +1030

    added chartlist item component

---
+++ client/src/components/features/charts/ChartListItem.tsx
@@ -0,0 +1,61 @@
+import { IoIosArrowRoundDown, IoIosArrowRoundUp } from "react-icons/io";
+import { Link } from "react-router-dom";
+
+interface Proptypes {
+  chart: Chart;
+  imageHeight?: number;
+  width?: number;
+}
+
+const ChartListItem = ({ chart, imageHeight, width }: Proptypes) => {
+  return (
+    <li
+      style={{ width: width ? width : "100%" }}
+      className="h-full inline-block p-2 cursor-pointer hover:bg-indigo-100 ease-in-out duration-300"
+    >
+      <Link to={`/chart/${chart._id}`}>
+        <div>
+          <div className="flex place-content-between">
+            <h1 className="truncate font-bold text-md text-black capitalize">
+              {chart.chartTitle}
+            </h1>
+            <div className="inline-flex gap-1">
+              <div className="flex">
+                <p className="text-[10px] text-center">
+                  {chart.upVotes.length}
+                </p>
+                <IoIosArrowRoundUp className="text-green-500" />
+              </div>
+              <div className="flex">
+                <p className="text-[10px] text-center">
+                  {chart.downVotes.length}
+                </p>
+                <IoIosArrowRoundDown className="text-red-500" />
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <div className="grid-flow-col">
+              <h1 className="truncate font-bold text-sm uppercase text-indigo-600">
+                {chart.coinName}
+              </h1>
+              <p className="truncate italic font-bold text-xs text-slate-500 w-[110px]">
+                By {chart.username}
+              </p>
+            </div>
+            <p className="text-gray-500 text-xs whitespace-normal text-right mt-auto">
+              Posted On: {chart.createdAt}
+            </p>
+          </div>
+        </div>
+        <img
+          style={{ height: imageHeight ? imageHeight : "fit" }}
+          src={chart.imageThumbnail}
+          className="w-full rounded-sm"
+        />
+      </Link>
+    </li>
+  );
+};
+
+export default ChartListItem;
