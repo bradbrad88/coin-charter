8d052d1fa7e405f34457fbc46f01e9049a7a52ae
Brad Teague <b_rad88@live.com>
2022-11-15 16:18:19 +1030

    Set database connection string ready for heroku env variables

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
 const { connect, connection } = require("mongoose");
 
 // On deployment will check for environment variable, if not then local
 // const connectionString =
-//   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/coinCharterDB';
+const url =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/coinCharterDB";
 
 // Create connection to MongoDB
-connect("mongodb://127.0.0.1:27017/coinCharterDB", {
+connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
