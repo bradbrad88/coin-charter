e516c4a8ea0c735ed656424109cd32cbc03e7c5a
Sammy <marchsam1988@gmail.com>
2022-11-15 11:14:47 +1030

    container now has width of full

--- client/src/components/common/Container.tsx
+++ client/src/components/common/Container.tsx
@@ -4,7 +4,7 @@ interface Proptypes {
 
 const Container = ({ children }: Proptypes) => {
   return (
-    <div className="md:shadow-2xl rounded-sm shadow-gray-900 h-full">
+    <div className="md:shadow-2xl rounded-sm shadow-gray-900 h-full w-full">
       {children}
     </div>
   );
