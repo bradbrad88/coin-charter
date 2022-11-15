a34a7001b2e6e1f07bba2bea4266bacef642eb70
Sammy <marchsam1988@gmail.com>
2022-11-15 11:25:15 +1030

    refactoring to use new components(shared) and clean up

--- client/src/components/features/charts/TopCharts.tsx
+++ client/src/components/features/charts/TopCharts.tsx
@@ -1,28 +1,16 @@
-import { useRef, useMemo } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useMutation, useQuery } from "@apollo/client";
-import { IoIosArrowRoundUp, IoIosArrowRoundDown } from "react-icons/io";
-import { TfiArrowCircleLeft, TfiArrowCircleRight } from "react-icons/tfi";
-import useUserContext from "contexts/UserContext";
+import { useMemo } from "react";
+import { useQuery } from "@apollo/client";
 import Container from "src/components/common/Container";
-import {
-  DOWNVOTE_CHART,
-  QUERY_ALL_CHARTS,
-  UPVOTE_CHART,
-} from "src/graphql/queries";
-import VoteWidget from "src/components/common/VoteWidget";
+import { QUERY_ALL_CHARTS } from "src/graphql/queries";
 import ChartListItem from "./ChartListItem";
+import SideScrollReel from "src/components/common/SideScrollReel";
 
 interface Query {
   charts: Chart[];
 }
 
 const MostRecent = () => {
-  const { user } = useUserContext();
-  const { data, refetch } = useQuery<Query>(QUERY_ALL_CHARTS);
-  const [upVoteMutation] = useMutation(UPVOTE_CHART);
-  const [downVoteMutation] = useMutation(DOWNVOTE_CHART);
-  const ref = useRef<HTMLUListElement>(null);
+  const { data } = useQuery<Query>(QUERY_ALL_CHARTS);
 
   const topRated = useMemo(() => {
     if (!data || data.charts.length < 1) return null;
@@ -45,36 +33,6 @@ const MostRecent = () => {
       ));
   }, [data]);
 
-  const upVote = useMemo(() => {
-    if (!topRated) return false;
-    return topRated.upVotes.some((vote) => vote._id === user?._id);
-  }, [data]);
-
-  const downVote = useMemo(() => {
-    if (!topRated) return false;
-    return topRated.downVotes.some((vote) => vote._id === user?._id);
-  }, [data]);
-
-  const handleUpVote = async (id: string, vote: boolean) => {
-    await upVoteMutation({ variables: { id, vote } });
-    refetch();
-  };
-
-  const handleDownVote = async (id: string, vote: boolean) => {
-    await downVoteMutation({ variables: { id, vote } });
-    refetch();
-  };
-
-  const slideLeft = () => {
-    if (!ref.current) return;
-    ref.current.scrollLeft = ref.current.scrollLeft - 300;
-  };
-
-  const slideRight = () => {
-    if (!ref.current) return;
-    ref.current.scrollLeft = ref.current.scrollLeft + 300;
-  };
-
   if (!topRated) {
     return <div>Loading...</div>;
   }
@@ -92,22 +50,7 @@ const MostRecent = () => {
         <h1 className="font-bold text-lg text-center p-5 bg-white border-b border-t">
           Top Charts
         </h1>
-        <div className="relative h-[240px]">
-          <ul
-            ref={ref}
-            className="h-full w-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-thin"
-          >
-            {topCharts}
-          </ul>
-          <TfiArrowCircleLeft
-            onClick={slideLeft}
-            className="absolute top-1/2 left-2 text-3xl text-white bg-primary rounded-full p-2 cursor-pointer hidden lg:block"
-          />
-          <TfiArrowCircleRight
-            onClick={slideRight}
-            className="absolute top-1/2 right-2 text-3xl text-white bg-primary rounded-full p-2 cursor-pointer hidden lg:block"
-          />
-        </div>
+        <SideScrollReel>{topCharts}</SideScrollReel>
       </div>
     </Container>
   );
--- client/src/components/features/coinProfile/TopCharts.tsx
+++ client/src/components/features/coinProfile/TopCharts.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { QUERY_COIN_CHART } from "src/graphql/queries";
+import ChartListItem from "../charts/ChartListItem";
 
 interface CoinId {
   coinId: string;
@@ -34,46 +34,15 @@ const TopCharts = ({ coinId }: CoinId) => {
   };
 
   return (
-    <div className="flex w-4/6 h-full">
-      <div className="border-r pr-4 flex flex-col w-full h-full">
-        <h1 className="text-lg font-bold">Top Rated Chart</h1>
-        {!topRatedChart ? (
-          <div className="h-full">
-            No charts have been made yet. Submit one above to be the first!
-          </div>
-        ) : (
-          <div className="flex flex-col h-full">
-            <div className="flex gap-3 items-center">
-              <h1 className="font-bold text-md text-indigo-600">
-                <p>{topRatedChart.chartTitle}</p>
-              </h1>
-              <p className="italic font-bold text-sm text-slate-500">
-                By{" "}
-                <Link to={`/profile/${topRatedChart.userId}`}>
-                  <span className="hover:cursor-pointer">
-                    {topRatedChart.username}
-                  </span>
-                </Link>
-              </p>
-              <div className="flex flex-col">
-                <i className="fa-regular fa-thumbs-up text-[14px]"></i>
-                <p className="text-[8px]">{topRatedChart.upVotes.length}</p>
-              </div>
-              <div className="flex flex-col">
-                <i className="fa-regular fa-thumbs-down text-[14px]"></i>
-                <p className="text-[8px]">{topRatedChart.downVotes.length}</p>
-              </div>
-            </div>
-            <p>{topRatedChart.chartDescription}</p>
-            <Link to={`/chart/${topRatedChart._id}`}>
-              <img
-                src={topRatedChart.imageThumbnail}
-                className="h-[400px] w-full mt-2 hover:brightness-50 hover:cursor-pointer"
-              />
-            </Link>
-          </div>
-        )}
-      </div>
+    <div className="flex flex-col w-full h-full">
+      <h1 className="text-lg font-bold">Top Rated Chart</h1>
+      {!topRatedChart ? (
+        <div className="h-full">
+          No charts have been made yet. Submit one above to be the first!
+        </div>
+      ) : (
+        <ChartListItem key={topRatedChart._id} chart={topRatedChart} />
+      )}
     </div>
   );
 };
