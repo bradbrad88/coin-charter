cb98800eab804fa233e9d421baf87a4b0622a379
Brad Teague <b_rad88@live.com>
2022-11-15 15:51:30 +1030

    Style chart comments

--- client/src/components/features/coinChart/Comments.tsx
+++ client/src/components/features/coinChart/Comments.tsx
@@ -5,6 +5,7 @@ import Button from "src/components/common/Button";
 import { Link } from "react-router-dom";
 import Container from "src/components/common/Container";
 import VoteWidget from "src/components/common/VoteWidget";
+import { toast } from "react-toastify";
 
 interface Proptypes {
   chart: Chart;
@@ -13,15 +14,22 @@ interface Proptypes {
 const CreateComment = ({ chart }: Proptypes) => {
   const chartId = chart?._id;
   const [commentInput, setCommentInput] = useState<string>("");
-  const [addChartComment, { error, data }] = useMutation(ADD_CHART_COMMENT);
+  const [addChartComment, { error }] = useMutation(ADD_CHART_COMMENT, {
+    refetchQueries: [
+      { query: QUERY_CHART_COMMENTS, variables: { chartId: chart._id } },
+    ],
+  });
 
-  const submitComment = () => {
+  const submitComment = async () => {
     const variables = {
       commentText: commentInput,
       chartId,
     };
 
-    addChartComment({ variables });
+    const res = await addChartComment({ variables });
+    if (!res) return toast.error("Couldn't upload your comment at this time");
+    toast.success("Comment added");
+    setCommentInput("");
   };
 
   if (error) {
@@ -29,31 +37,30 @@ const CreateComment = ({ chart }: Proptypes) => {
   }
 
   return (
-    <div className="p-5">
-      <h1 className="text-center">Add Comment:</h1>
-      <form>
+    <form className="flex flex-col">
+      <label className="block">
+        Add a comment
         <textarea
-          className="w-full text-sm bg-slate-200"
+          className="w-full h-full border-[1px] border-gray-500 rounded-sm"
           placeholder="Add text here..."
           rows={3}
           value={commentInput}
           onChange={(e) => setCommentInput(e.target.value)}
         ></textarea>
-        <Button onClick={submitComment}>Submit</Button>
-      </form>
-    </div>
+      </label>
+      <Button disabled={!commentInput} onClick={submitComment}>
+        Submit
+      </Button>
+    </form>
   );
 };
 
 const Comments = ({ chart }: Proptypes) => {
   const chartId = chart?._id;
   const [commentsList, setCommentsList] = useState<any>();
-  const { loading, error, data } = useQuery<{ chartComments: Chart }>(
-    QUERY_CHART_COMMENTS,
-    {
-      variables: { chartId },
-    },
-  );
+  const { data } = useQuery<{ chartComments: Chart }>(QUERY_CHART_COMMENTS, {
+    variables: { chartId },
+  });
   useEffect(() => {
     let chartData = data?.chartComments;
 
@@ -62,54 +69,61 @@ const Comments = ({ chart }: Proptypes) => {
     }
   }, [data]);
 
-  console.log(commentsList);
-
   if (!commentsList) {
     return <div>Loading...</div>;
   }
 
   return (
     <Container>
-      <div className="flex flex-col">
-        <h1 className="text-lg font-bold text-center">Comments</h1>
+      <div className="flex flex-col gap-3 px-5">
+        <h1 className="text-xl font-semibold text-gray-500">Comments</h1>
         <CreateComment chart={chart} />
-        <ul>
-          {commentsList.map((info: any, index: number) => (
-            <li
-              key={info.username + index}
-              className="border-black-50 border-solid border-2 flex flex-row justify-between h-[92px]"
-            >
-              <div className="flex">
-                <div className="">
-                  <img src={info.image} className="h-[50px] w-[50px] rounded" />
-                  <div className="">
-                    <Link to={`/profile/${info.userId}`}>
-                      <span className="font-bold text-sm text-indigo-600">
-                        {info.username}
-                      </span>
-                    </Link>
+        <Container>
+          <ul className="max-h-[80vh] overflow-y-auto">
+            {commentsList.map((info: any, index: number) => (
+              <li
+                key={info.username + index}
+                className="border-black-5000 flex flex-row justify-between gap-3 p-2"
+              >
+                <div className="flex gap-2 p-2 w-full bg-gray-200 rounded-lg">
+                  <div className="flex flex-col w-full gap-2">
+                    <div className="flex justify-between">
+                      <div
+                        className="
+                    flex gap-2"
+                      >
+                        <div className="shrink-0 w-12 h-12 rounded-full overflow-hidden border-2 border-primary bg-white">
+                          <img src={info.image} className="h-full w-full" />
+                        </div>
+                        <div className="">
+                          <Link to={`/profile/${info.userId}`}>
+                            <span className="font-semibold text-indigo-600">
+                              {info.username}
+                            </span>
+                          </Link>
+                          <p className="text-gray-500 text-sm leading-none">
+                            {info.createdAt}
+                          </p>
+                        </div>
+                      </div>
+                      <VoteWidget
+                        handleUpVote={(vote) => {}}
+                        handleDownVote={(vote) => {}}
+                        downVote={true}
+                        upVote={true}
+                        downVoteCount={1}
+                        upVoteCount={2}
+                      />
+                    </div>
+                    <div className="bg-white p-2 rounded-md">
+                      <p className="">{info.commentText}</p>
+                    </div>
                   </div>
                 </div>
-                <div>
-                  <p className="text-xs p-5">{info.commentText}</p>
-                </div>
-              </div>
-              <div className="flex flex-col justify-between p-1">
-                <VoteWidget
-                  handleUpVote={(vote) => {}}
-                  handleDownVote={(vote) => {}}
-                  downVote={true}
-                  upVote={true}
-                  downVoteCount={1}
-                  upVoteCount={2}
-                />
-                <p className="text-gray-500 text-xs ml-auto">
-                  {info.createdAt}
-                </p>
-              </div>
-            </li>
-          ))}
-        </ul>
+              </li>
+            ))}
+          </ul>
+        </Container>
       </div>
     </Container>
   );
