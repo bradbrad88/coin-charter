c81eedafbeeb94dcffa58dba23eaddfbda3105dc
Brad Teague <b_rad88@live.com>
2022-11-15 16:07:55 +1030

    Implement chart voting

--- client/src/components/features/coinChart/MainChart.tsx
+++ client/src/components/features/coinChart/MainChart.tsx
@@ -1,15 +1,43 @@
+import { useMutation } from "@apollo/client";
+import { useMemo } from "react";
 import { Link } from "react-router-dom";
 import Container from "src/components/common/Container";
 import VoteWidget from "src/components/common/VoteWidget";
-
+import { DOWNVOTE_CHART, QUERY_CHART, UPVOTE_CHART } from "src/graphql/queries";
+import useUserContext from "contexts/UserContext";
 interface Proptypes {
   chart: Chart;
 }
 
 const MainChart = ({ chart }: Proptypes) => {
+  const { user } = useUserContext();
+  const [upVoteChart] = useMutation(UPVOTE_CHART, {
+    refetchQueries: [{ query: QUERY_CHART, variables: { chartId: chart._id } }],
+  });
+  const [downVoteChart] = useMutation(DOWNVOTE_CHART, {
+    refetchQueries: [{ query: QUERY_CHART, variables: { chartId: chart._id } }],
+  });
+
   if (!chart) {
     return <div>Loading...</div>;
   }
+
+  const upVote = useMemo(() => {
+    if (!user) return false;
+    return chart.upVotes.some((vote) => vote._id === user._id);
+  }, [chart.upVotes]);
+
+  const downVote = useMemo(() => {
+    if (!user) return false;
+    return chart.downVotes.some((vote) => vote._id === user._id);
+  }, [chart.downVotes]);
+
+  const handleUpVote = (vote: boolean) => {
+    upVoteChart({ variables: { id: chart._id, vote } });
+  };
+  const handleDownVote = (vote: boolean) => {
+    downVoteChart({ variables: { id: chart._id, vote } });
+  };
 
   return (
     <Container>
@@ -40,12 +68,12 @@ const MainChart = ({ chart }: Proptypes) => {
             <div className="flex flex-col lg:flex-row lg:gap-5 lg:p-3">
               <div className="flex lg:gap-5 lg:pl-2">
                 <VoteWidget
-                  handleUpVote={(vote) => {}}
-                  handleDownVote={(vote) => {}}
-                  downVote={true}
-                  upVote={true}
-                  downVoteCount={1}
-                  upVoteCount={2}
+                  handleUpVote={handleUpVote}
+                  handleDownVote={handleDownVote}
+                  downVote={downVote}
+                  upVote={upVote}
+                  downVoteCount={chart.downVoteCount}
+                  upVoteCount={chart.upVoteCount}
                 />
               </div>
               <h1 className="text-sm text-gray-500 mt-auto">
