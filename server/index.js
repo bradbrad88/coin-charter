5c86c77209c5b7fe12cfc9e014ab08638c12ab6d
Brad Teague <b_rad88@live.com>
2022-11-19 13:16:51 +1030

    Refactor server to handle websockets and use express middleware to host graphql endpoint

--- server/index.js
+++ server/index.js
@@ -1,14 +1,33 @@
 require("dotenv").config();
-const express = require("express");
-const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
+const { createServer } = require("http");
+const { WebSocketServer } = require("ws");
+const { ApolloServer } = require("@apollo/server");
+const { useServer } = require("graphql-ws/lib/use/ws");
 const db = require("./config/connection");
+const express = require("express");
+const { expressMiddleware } = require("@apollo/server/express4");
+const { gqlAuthMiddleware, wsAuthMiddleware } = require("./utils/auth");
+const cors = require("cors");
+const { json } = require("body-parser");
+const {
+  ApolloServerPluginDrainHttpServer,
+} = require("@apollo/server/plugin/drainHttpServer");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
 const { typeDefs, resolvers } = require("./schemas");
-const { gqlAuthMiddleware } = require("./utils/auth");
 const router = require("./routes");
 
 // Create express app
 const app = express();
+
+// Create a http server that handles express app as well as websocket connection required for subscriptions
+const httpServer = createServer(app);
+
+// Create web socket server to handle subscription events over graphql
+const wsServer = new WebSocketServer({
+  server: httpServer,
+  path: "/graphql",
+});
 
 // Default to port 3001 if not port specified in env variables
 const PORT = process.env.PORT || 3001;
@@ -16,43 +35,54 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(require("cookie-parser")());
-
 app.use(express.static(path.join(__dirname, "../client/dist")));
-
-// Serve the custom api routes
-app.use(router);
-
-// Serve the react client
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-});
-
-app.get("*", (req, res, next) => {
-  const file = path.join(__dirname, "../client/dist/index.html");
-  res.sendFile(file);
-});
 
 // Function for starting the apollo server
 const startApolloServer = async (typeDefs, resolvers, context) => {
+  //
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+  // Handle  shutdown of webserver
+  const serverCleanup = useServer(
+    { schema, context: wsAuthMiddleware },
+    wsServer,
+  );
+
   // Create apollo server with typeDefs, resolvers and context middleware
   const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context,
+    schema,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose();
+            },
+          };
+        },
+      },
+    ],
   });
 
+  // Apollo server must be started before passing to expressMiddleware
   await server.start();
 
-  server.applyMiddleware({ app });
+  // Serve graphql queries
+  app.use("/graphql", cors(), json(), expressMiddleware(server, { context }));
 
-  // Once db connection has been established, set the app to listen on specified port
-  db.once("open", () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(
-        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`,
-      );
-    });
+  // Serve the custom api routes
+  app.use(router);
+
+  // Serve the react client
+  app.get("*", (req, res) => {
+    const file = path.join(__dirname, "../client/dist/index.html");
+    res.sendFile(file);
+  });
+
+  db.once("open", async () => {
+    await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve));
+    console.log(`Server listening on port ${PORT}`);
   });
 };
 
