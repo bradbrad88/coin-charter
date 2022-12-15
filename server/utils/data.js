98e5666839e18dc13a475228a398d50dae432044
Brad Teague <b_rad88@live.com>
2022-12-15 19:00:53 +1030

    Seed user data and friends

--- server/utils/data.js
+++ server/utils/data.js
@@ -1,24 +1,29 @@
 const userSeed = [
   {
     username: "Bennyboy",
-    password: "12345678",
-    subTitle: "Beginner Crypto",
-    email: "bennyboy@google.com",
+    // password: "12345678",
+    subtitle: "Beginner Crypto",
+    // email: "bennyboy@google.com",
     bio: "I am a beginner trader and enjoy searching up new crypto coins",
   },
   {
-    username: "Braddy",
-    password: "12345678",
-    subTitle: "Lover of coins",
-    email: "braddy@test.com",
+    username: "Bradddy",
+    // password: "12345678",
+    subtitle: "Lover of coins",
+    // email: "braddy@test.com",
     bio: "I am very excited to learn more about how charts work and start using them more.",
   },
   {
     username: "Sammy",
-    password: "12345678",
-    subTitle: "Expertish",
-    email: "sammy@google.com",
+    // password: "12345678",
+    subtitle: "Expertish",
+    // email: "sammy@google.com",
     bio: "Crypto is very interesting and i am keen to get into the space.",
+  },
+  {
+    username: "Jo",
+    subtitle: "St Cryptos",
+    bio: "I'm bored",
   },
 ];
 
--- server/utils/seed.js
+++ server/utils/seed.js
@@ -1,83 +1,38 @@
-// Import connection, models and seed data
-const connection = require("../config/connection");
-const { Users, Coins, Charts, Comments } = require("../models");
-const { userSeed, coinSeed, chartSeed } = require("./data");
+const prisma = require("../config/connection");
+const { userSeed } = require("./data");
 
-connection.on("error", (err) => err);
+async function main() {
+  await prisma.user.deleteMany();
 
-// Create connection and load seed data into the models
-connection.once("open", async () => {
-  console.log("Connected");
-  await Coins.deleteMany({});
+  const users = await Promise.all(
+    userSeed.map(async (user) => {
+      const newUser = await prisma.user.create({ data: user });
+      return newUser;
+    }),
+  );
 
-  const coinPromises = coinSeed.map(async (coin) => {
-    return await Coins.create(coin);
+  await prisma.user.update({
+    where: { id: users[0].id },
+    data: {
+      UserFriends: { create: { friendId: users[1].id } },
+      friends: { create: { userId: users[1].id } },
+    },
   });
 
-  const coins = await Promise.all(coinPromises);
-
-  // Drop any existing documents from models
-  await Users.deleteMany({});
-  await Comments.deleteMany({});
-
-  // Insert new dataseed into models
-
-  const userPromises = userSeed.map(async (user) => {
-    return await Users.create({
-      ...user,
-      favCoins: [{ coin: coins[Math.floor(Math.random() * coins.length)] }],
-    });
+  await prisma.user.update({
+    where: { id: users[2].id },
+    data: {
+      UserFriends: { create: { friendId: users[0].id } },
+      friends: { create: { userId: users[0].id } },
+    },
   });
-
-  const users = await Promise.all(userPromises);
-
-  const friendPromises = users.map(async (user, idx) => {
-    const id = users[(idx + 1) % users.length]._id;
-    return await user.updateOne(
-      {
-        $push: {
-          friends: {
-            $each: [{ friend: id }],
-            $position: 0,
-          },
-        },
-      },
-      { timestamps: true, new: true },
-    );
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
   });
-
-  const newUsers = await Promise.all(friendPromises);
-
-  await Charts.deleteMany({});
-
-  const mappedCharts = chartSeed.map((chart) => {
-    const { _id, username } = users[Math.floor(Math.random() * users.length)];
-
-    return {
-      ...chart,
-      userId: _id,
-      username,
-    };
-  });
-
-  // console.log(mappedCharts);
-  await Charts.insertMany(mappedCharts);
-
-  const updateCoins = await Charts.find({});
-
-  const coinsUpdateCharts = updateCoins.map(async (chart) => {
-    const { coinId } = chart;
-
-    return await Coins.findOneAndUpdate(
-      { coinId },
-      { $push: { coinCharts: chart._id } },
-      { new: true },
-    );
-  });
-  const chartCoins = await Promise.all(coinsUpdateCharts);
-
-  console.log(coinsUpdateCharts);
-
-  console.log("Seed complete");
-  process.exit();
-});
